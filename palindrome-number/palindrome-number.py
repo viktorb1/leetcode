a9904388d18c441399f546c9ea7b641303ba518e
Viktor Babchanik <vbabchanik@ucdavis.edu>
2021-07-20 23:57:29 -0700

    Time: 60 ms (69.43%), Space: 14 MB (92.23%) - LeetHub

--- palindrome-number/palindrome-number.py
+++ palindrome-number/palindrome-number.py
@@ -1,27 +1,12 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
+        val = str(x)
         
-        nums = []
-        
-        # create an array out of the integer
-        while x > 0:
-            nums.append(x % 10)
-            x //= 10
-        
-        # loop through half the array, check if the first and last int is the same
-        for i in range(len(nums) // 2):
-            end = len(nums) - i - 1
-            
-            if nums[i] != nums[end]:
+        for i in (range(len(val)//2)):
+            if (val[i] == val[len(val)-i-1]):
+                continue
+            else:
                 return False
         
-            end -= 1
-        
-        
         return True
-            
-            
-            
             
