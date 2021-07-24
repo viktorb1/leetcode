251fa8868b077ec9a38993b06efba64662d30ece
Viktor Babchanik <vbabchanik@ucdavis.edu>
2021-07-24 02:28:54 -0700

    Update remove-element.py

--- remove-element/remove-element.py
+++ remove-element/remove-element.py
@@ -1,11 +1,14 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-            try:
-                while True:
-                    nums.remove(val)
-            except ValueError:
-                    pass
-                
+        
+        curr = 0
+        
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[curr] = nums[i]
+                curr += 1       
+
+        return curr
     
 # ALTERNATIVE SOLUTION:
 # class Solution:
