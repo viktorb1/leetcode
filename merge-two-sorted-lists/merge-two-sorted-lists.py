9e3d1828a6abeff0f3ee6a2101fc1a8d2cb86bb0
Viktor Babchanik <vbabchanik@ucdavis.edu>
2021-07-24 00:07:52 -0700

    Update merge-two-sorted-lists.py

--- merge-two-sorted-lists/merge-two-sorted-lists.py
+++ merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -9,13 +9,6 @@ class Solution:
         newList = ListNode(0)
         curr = newList
         
-        if not l1 and not l2:
-            return None
-        elif not l1:
-            return l2
-        elif not l2:
-            return l1
-        
         while l1 and l2:
             if l1.val < l2.val:
                 curr.next = l1
