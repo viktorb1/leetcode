2cba84ffeb6930424f36a353a25cc2a614a91249
Viktor Babchanik <vbabchanik@ucdavis.edu>
2021-07-26 11:54:26 -0700

    Time: 36 ms (49.39%), Space: 14.2 MB (53.68%) - LeetHub

---
+++ add-binary/add-binary.py
@@ -0,0 +1,33 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        i = len(a) - 1
+        j = len(b) - 1
+        remainder = 0
+        newStr = ""
+
+        
+        while i >= 0 or j >= 0:
+            total = 0
+
+            if i >= 0 and j >= 0:
+                total = int(a[i]) + int(b[j]) + remainder
+            elif i >= 0:
+                total = int(a[i]) + remainder
+            else:
+                total = int(b[j]) + remainder
+            
+            newStr = str(total % 2) + newStr
+            
+            if total > 1:
+                remainder = 1
+            else:
+                remainder = 0
+                
+            i -= 1
+            j -= 1
+            
+        
+        if remainder == 1:
+            newStr = "1" + newStr
+                
+        return newStr
