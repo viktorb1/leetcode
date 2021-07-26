18dd3c8ac6922beb977384d675318d88bd94ac7d
Viktor Babchanik <vbabchanik@ucdavis.edu>
2021-07-26 14:20:42 -0700

    Time: 32 ms (88.97%), Space: 14.2 MB (39.74%) - LeetHub

--- sqrtx/sqrtx.py
+++ sqrtx/sqrtx.py
@@ -1,12 +1,18 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        i = 1
         
-        while i <= x:
+        low = 1
+        high = x
+        i = 0
+        
+        while high - low >= 0:
+            i = (low + high) // 2
+
             if i ** 2 > x:
+                high = i - 1
+            elif (i+1) ** 2 > x:
                 break
             else:
-                i += 1
+                low = i + 1
                 
-        
-        return i - 1
+        return i
