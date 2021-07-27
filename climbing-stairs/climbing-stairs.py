5ba86e3dec3f8c04c5bb51d9acc8777ea7485fa9
Viktor Babchanik <vbabchanik@ucdavis.edu>
2021-07-26 18:33:03 -0700

    Time: 20 ms (99.15%), Space: 14.4 MB (11.80%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.py
@@ -0,0 +1,17 @@
+class Solution:
+    def climbStairsHelper(self, i: int, n: int, memo: List[int]) -> int:
+         
+        if i > n: # we went out of bounds, so it's not a valid sequence
+            return 0
+        elif i == n:
+            return 1 # we stayed in bounds, so it's a valid sequence to climb the stairs
+        elif memo[i] > 0: # save previously calculated values, 'memoization'
+            return memo[i]
+        else:
+            memo[i] = self.climbStairsHelper(i+1, n, memo) + self.climbStairsHelper(i+2, n, memo)
+            return memo[i]
+        
+    def climbStairs(self, n: int) -> int:
+        memo = [0] * n
+        return self.climbStairsHelper(0, n, memo)
+        
