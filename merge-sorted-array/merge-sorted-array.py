801ec760653e1f070c3b3f2950301af329e98f7d
Viktor Babchanik <vbabchanik@ucdavis.edu>
2021-07-26 20:19:40 -0700

    Update merge-sorted-array.py

--- merge-sorted-array/merge-sorted-array.py
+++ merge-sorted-array/merge-sorted-array.py
@@ -3,18 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        start = 0
 
-        for j in range(0, n): 
+        for j in range(0, n):
             
-            i = 0
+            i = start #since the array is sorted, you don't have to check anything before what you've checked already 
             
             while i < m and nums1[i] < nums2[j]:
                 i +=  1
+            
+            start = i + 1
 
             for k in range(m-1, i-1, -1):
                 nums1[k+1] = nums1[k]
 
             nums1[i] = nums2[j]
             m += 1
-                
-            
