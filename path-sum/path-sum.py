d33a21db969d1e7b977bfb42253e541fe0f57738
Viktor Babchanik <vbabchanik@ucdavis.edu>
2021-07-29 01:21:33 -0700

    Update path-sum.py

--- path-sum/path-sum.py
+++ path-sum/path-sum.py
@@ -7,15 +7,13 @@
 class Solution:
     
     def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
-        
-        # traverse tree
-        # keep adding while less than targetSum or null
+
         if not root:
             return False
         
         targetSum -= root.val
         
-        if targetSum == 0 and not root.left and not root.right:
+        if targetSum == 0 and not root.left and not root.right: # check to make sure it was a leafnode
             return True
          
         return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)     
