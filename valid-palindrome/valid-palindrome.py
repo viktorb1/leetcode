2ef6f4e34af3416388cdea5639eb0ba0413c8849
Viktor Babchanik <vbabchanik@ucdavis.edu>
2021-08-01 02:00:34 -0700

    Update valid-palindrome.py

--- valid-palindrome/valid-palindrome.py
+++ valid-palindrome/valid-palindrome.py
@@ -1,14 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
+        st = ""
+        
         for i in s:
-            if not i.isalnum():
-                s = s.replace(i, "")
+            if i.isalnum():
+                st += i.lower()
         
-        s = s.lower()
-        
-        for i in range(len(s) // 2):
-            if s[i] != s[len(s) - i - 1]:
+        for i in range(len(st) // 2):
+            if st[i] != st[len(st) - i - 1]:
                 return False
         
         return True
