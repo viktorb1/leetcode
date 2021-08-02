5e5f8b2105a70c6c957c07766ea38f1a371b1e8d
Viktor Babchanik <vbabchanik@ucdavis.edu>
2021-08-01 21:01:39 -0700

    Update excel-sheet-column-title.py

--- excel-sheet-column-title/excel-sheet-column-title.py
+++ excel-sheet-column-title/excel-sheet-column-title.py
@@ -2,12 +2,10 @@ class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
                         
         sol = ""
-        i = 0
         
         while columnNumber > 0:
             sol += chr(ord('A') + (columnNumber - 1) % 26)
             columnNumber -= (columnNumber - 1) % 26
             columnNumber //= 26
-            i += 1
         
         return sol[::-1]
