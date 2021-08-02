dcd7d4d05a3de8caafc2b7425d64cad79a7c6cf2
Viktor Babchanik <vbabchanik@ucdavis.edu>
2021-08-01 23:38:30 -0700

    Update factorial-trailing-zeroes.py

--- factorial-trailing-zeroes/factorial-trailing-zeroes.py
+++ factorial-trailing-zeroes/factorial-trailing-zeroes.py
@@ -1,6 +1,5 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-        numtwos = 0
         numfives = 0
         numtens = 0
         
@@ -11,7 +10,5 @@ class Solution:
             while i % 5 == 0:
                 numfives += 1
                 i //= 5
-            if i % 2 == 0:
-                numtwos += i // 2
 
-        return min(numtwos, numfives) + numtens
+        return numfives + numtens
