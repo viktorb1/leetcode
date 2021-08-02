e479fb783b8a1d741975bef77f7914824771a74c
Viktor Babchanik <vbabchanik@ucdavis.edu>
2021-08-01 22:31:50 -0700

    Update majority-element.py

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -11,3 +11,21 @@ class Solution:
         for val in count:
             if count[val] > len(nums) // 2:
                 return val
+            
+# Alternative solution using Boyer-Moore Voting Algorithm
+# 
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         curr = nums[0]
+#         count = 0
+        
+#         for i in nums:
+#             if curr != i and count > 1:
+#                 count -= 1
+#             elif curr != i:
+#                 curr = i
+#                 count = 1
+#             else:
+#                 count += 1
+            
+#         return curr
