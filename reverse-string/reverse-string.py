67f2efff1f0596ee604a977f7ad888d217620894
Viktor Babchanik <vbabchanik@ucdavis.edu>
2021-08-02 23:06:20 -0700

    Update reverse-string.py

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -1,11 +1,4 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        
         for i in range(len(s) // 2):
-            temp = s[i]
-            s[i] = s[len(s) - i - 1]
-            s[len(s) - i - 1] = temp
-        
+            s[i], s[len(s)-i-1] = s[len(s)-i-1], s[i]
