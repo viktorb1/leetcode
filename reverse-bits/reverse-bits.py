506c4fa12395f3a3cd56566cc348a251665af7b3
Viktor Babchanik <vbabchanik@ucdavis.edu>
2021-08-05 01:44:21 -0700

    Time: 36 ms (40.19%), Space: 14.3 MB (39.67%) - LeetHub

---
+++ reverse-bits/reverse-bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseBits(self, n):
+        ans = 0
+        
+        for i in range(32):
+            ans <<= 1 
+            ans += (n & 1)
+            n >>= 1
+            
+        return ans
