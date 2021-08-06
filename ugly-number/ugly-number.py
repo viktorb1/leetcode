579a16bbaecc69b8aaa86d2ec536aac4fc5c9050
Viktor Babchanik <vbabchanik@ucdavis.edu>
2021-08-06 02:54:50 -0700

    Update ugly-number.py

--- ugly-number/ugly-number.py
+++ ugly-number/ugly-number.py
@@ -4,13 +4,13 @@ class Solution:
             return False
         
         while n % 2 == 0:
-            n /= 2
+            n //= 2
             
         while n % 3 == 0:
-            n /= 3
+            n //= 3
             
         while n % 5 == 0:
-            n /= 5
+            n //= 5
             
         if n == 1:
             return True   
