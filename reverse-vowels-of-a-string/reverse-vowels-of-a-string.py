ab6e334645466f0838d89089c094eefc1e5baf66
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-08-07 13:37:25 -0700

    Update reverse-vowels-of-a-string.py

--- reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -2,7 +2,7 @@ class Solution:
     def reverseVowels(self, s: str) -> str:
         start = 0
         end = len(s) - 1
-#         vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+#       vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
         vowels = {'a':1, 'e':1, 'i':1, 'o':1, 'u':1, 'A':1, 'E':1, 'I':1, 'O':1, 'U':1}
         ls = list(s)
         
