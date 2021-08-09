629706f20d3601a7cef3f23eb37e71b8f67341e1
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-08-09 11:34:28 -0700

    Update summary-ranges.py

--- summary-ranges/summary-ranges.py
+++ summary-ranges/summary-ranges.py
@@ -7,15 +7,13 @@ class Solution:
         ranges = []
         
         for i in range(len(nums)):
-            if i == len(nums) - 1 or nums[i] + 1 < nums[i+1]:
+            if i == len(nums) - 1 or nums[i] + 1 < nums[i+1]: # last element or next element is greater by more than one
                 if start == nums[i]:
                     ranges.append(str(start))
                 else:
                     ranges.append(str(start) + '->' + str(nums[i]))
                     
-                if len(nums) > i+1:
+                if len(nums) > i+1: # set next start element
                     start = nums[i+1]
-
             
         return ranges
-            
