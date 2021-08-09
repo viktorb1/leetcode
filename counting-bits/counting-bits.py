51244b00fdf21b9540fa5e780eda933d17b78bf6
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-08-08 23:40:00 -0700

    Time: 208 ms (15.40%), Space: 20.8 MB (91.19%) - LeetHub

---
+++ counting-bits/counting-bits.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        out = [0]
+        
+        for i in range(1, n+1):
+            total = 0
+            
+            while i > 0:
+                if i % 2 == 1:
+                    total +=1
+                
+                i //= 2
+                    
+            out.append(total)
+            
+        
+        return out
+            
+            
