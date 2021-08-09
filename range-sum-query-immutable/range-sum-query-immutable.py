01b782482fb1fe1fb627c39186a4f8f2f6cd3ce4
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-08-09 10:23:13 -0700

    Update range-sum-query-immutable.py

--- range-sum-query-immutable/range-sum-query-immutable.py
+++ range-sum-query-immutable/range-sum-query-immutable.py
@@ -8,9 +8,9 @@ class NumArray:
         
 
     def sumRange(self, left: int, right: int) -> int:     
-        return self.nums[right] - self.nums[left - 1]
+        return self.nums[right] - (self.nums[left - 1] if left > 0 else 0)
 
-
+    
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(left,right)
