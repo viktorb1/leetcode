1ef08c8e11e8098a5edb7e2308dbb6f4699dbd97
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-08-08 19:03:00 -0700

    Update find-the-difference.py

--- find-the-difference/find-the-difference.py
+++ find-the-difference/find-the-difference.py
@@ -1,17 +1,32 @@
+# class Solution:
+#     def findTheDifference(self, s: str, t: str) -> str:
+                
+#         counts = {}
+        
+#         for i in t:
+#             if i not in counts:
+#                 counts[i] = 1
+#             else:
+#                 counts[i] += 1
+        
+#         for i in s:
+#             counts[i] -= 1
+        
+#         for i in counts:
+#             if counts[i] == 1:
+#                 return i
+         
+    
+# Cleaner solution
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-                
-        counts = {}
-        
+    
+        sum = 0
+
         for i in t:
-            if i not in counts:
-                counts[i] = 1
-            else:
-                counts[i] += 1
-        
+            sum += ord(i)
+
         for i in s:
-            counts[i] -= 1
+            sum -= ord(i)
         
-        for i in counts:
-            if counts[i] == 1:
-                return i
+        return chr(sum)
