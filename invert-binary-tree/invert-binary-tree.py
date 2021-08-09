05386bbd535b02f3476953d50a80121ffff8c278
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-08-09 11:54:22 -0700

    Update invert-binary-tree.py

--- invert-binary-tree/invert-binary-tree.py
+++ invert-binary-tree/invert-binary-tree.py
@@ -9,11 +9,7 @@ class Solution:
         if not root:
             return None
         
-        temp = root.left
-        root.left = root.right
-        root.right = temp
-        
+        root.left, root.right = root.right, root.left
         self.invertTree(root.left)
         self.invertTree(root.right)
-        
         return root
