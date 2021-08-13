7368335f3417a42f085d79f1d972427dccd29aaf
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-08-12 19:42:58 -0700

    Remove sqrt() operation and replace with multiplication

--- construct-the-rectangle/construct-the-rectangle.py
+++ construct-the-rectangle/construct-the-rectangle.py
@@ -2,11 +2,15 @@ class Solution:
     def constructRectangle(self, area: int) -> List[int]:
         sol = []
         
-        for i in range(1, ceil(sqrt(area))+1):
+        i = 1
+        
+        while i*i <= area:
             if area % i == 0:
                 if (i > area // i):
                     sol.append([i , area // i])
                 else:
                     sol.append([area // i, i])
+            i += 1
                 
+        
         return sol[-1]
