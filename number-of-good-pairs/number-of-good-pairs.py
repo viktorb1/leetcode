871e739f9b235efc64068f2bfae73a9dbaec9728
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-08-14 12:39:17 -0700

    Time: 32 ms (75.89%), Space: 14.3 MB (44.39%) - LeetHub

--- number-of-good-pairs/number-of-good-pairs.py
+++ number-of-good-pairs/number-of-good-pairs.py
@@ -1,10 +1,9 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         count = 0
+        numCount = collections.Counter(nums)
         
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] == nums[j]:
-                    count += 1
-                    
+        for i in numCount:
+            count += (numCount[i])*(numCount[i]-1) // 2
+
         return count
