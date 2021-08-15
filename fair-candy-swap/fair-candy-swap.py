0c0d3caa215ca973be300afd16790170df10dbd9
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-08-15 10:40:13 -0700

    Update fair-candy-swap.py

--- fair-candy-swap/fair-candy-swap.py
+++ fair-candy-swap/fair-candy-swap.py
@@ -1,16 +1,14 @@
 class Solution:
     def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
-        aliceCount = 0
-        bobCount = 0
+        aliceCount = sum(aliceSizes)
+        bobCount = sum(bobSizes)
         alice = set()
         bob = set()
         
         for i in aliceSizes:
-            aliceCount += i
             alice.add(i)
             
         for i in bobSizes:
-            bobCount += i
             bob.add(i)
         
         diff = (bobCount - aliceCount) // 2
