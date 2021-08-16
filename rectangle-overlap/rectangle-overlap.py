2077cb34b5c2d1a82abd78adea0e99fec1164a67
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-08-16 11:36:12 -0700

    Update rectangle-overlap.py

--- rectangle-overlap/rectangle-overlap.py
+++ rectangle-overlap/rectangle-overlap.py
@@ -1,11 +1,14 @@
 class Solution:
     def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        # make sure no rectangles are actually lines
         if rec1[0] == rec1[2] or rec1[1] == rec1[3] or rec2[0] == rec2[2] or rec2[1] == rec2[3]:
             return False
         
+        # get the highest point and see if it's lower than the lowest point
         if rec2[2] <= rec1[0] or rec2[3] <= rec1[1]:
             return False
         
+        # get the lowest point and see if it's higher than the highest point
         if rec2[0] >= rec1[2] or rec2[1] >= rec1[3]:
             return False
         
