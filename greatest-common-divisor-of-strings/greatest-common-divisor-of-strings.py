ce63d2a095b6861bd1289e2cf7ab5cee2e3a72e7
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-08-18 19:51:20 -0700

    Update greatest-common-divisor-of-strings.py

--- greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,21 +1,22 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         pattern = ""
-        shorter = str2 if len(str2) < len(str1) else str1
-        longer = str2 if len(str2) >= len(str1) else str1
         
-        for i in range(1, len(shorter) + 1):
-            if len(shorter) % i != 0 or len(longer) % i != 0:
+        if len(str2) > len(str1):
+            str1, str2 = str2, str1
+        
+        for i in range(len(str2), 0, -1):
+            if len(str2) % i != 0 or len(str1) % i != 0:
                 continue
             
-            for j in range(0, len(shorter), i):
-                if shorter[j:j+i] != shorter[:i]:
+            for j in range(i, len(str2), i):
+                if str2[j:j+i] != str2[:i]:
                     break;
             else:
-                for j in range(0, len(longer), i):
-                    if longer[j:j+i] != shorter[:i]:
+                for j in range(0, len(str1), i):
+                    if str1[j:j+i] != str2[:i]:
                         break
-                    elif j == len(longer)-i:
-                        pattern = shorter[:i]
+                    elif j == len(str1)-i:
+                        return str2[:i]
 
-        return pattern
+        return ""
