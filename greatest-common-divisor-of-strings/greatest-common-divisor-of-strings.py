c5747549b889081dd0f0ab2a52f86312772765f2
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-08-18 19:28:42 -0700

    Time: 32 ms (68.50%), Space: 14.3 MB (58.05%) - LeetHub

---
+++ greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -0,0 +1,27 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        pattern = ""
+        shorter = str2 if len(str2) < len(str1) else str1
+        longer = str2 if len(str2) >= len(str1) else str1
+        isValidPattern = True
+        
+        for i in range(1, len(shorter) + 1):
+            if len(shorter) % i != 0:
+                continue
+            
+            for j in range(0, len(shorter), i):
+                if shorter[j:j+i] != shorter[:i]:
+                    isValidPattern = False
+                    break
+                    
+            if not isValidPattern:
+                isValidPattern = True
+                continue
+                    
+            for j in range(0, len(longer), i):
+                if longer[j:j+i] != shorter[:i]:
+                    break
+                elif j == len(longer)-i:
+                    pattern = shorter[:i]
+
+        return pattern
