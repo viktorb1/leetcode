c52f513d33d31731c9e6a7ae99061ff37105e480
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-08-18 19:42:11 -0700

    Update greatest-common-divisor-of-strings.py

--- greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -3,25 +3,19 @@ class Solution:
         pattern = ""
         shorter = str2 if len(str2) < len(str1) else str1
         longer = str2 if len(str2) >= len(str1) else str1
-        isValidPattern = True
         
         for i in range(1, len(shorter) + 1):
-            if len(shorter) % i != 0:
+            if len(shorter) % i != 0 or len(longer) % i != 0:
                 continue
             
             for j in range(0, len(shorter), i):
                 if shorter[j:j+i] != shorter[:i]:
-                    isValidPattern = False
-                    break
-                    
-            if not isValidPattern:
-                isValidPattern = True
-                continue
-                    
-            for j in range(0, len(longer), i):
-                if longer[j:j+i] != shorter[:i]:
-                    break
-                elif j == len(longer)-i:
-                    pattern = shorter[:i]
+                    break;
+            else:
+                for j in range(0, len(longer), i):
+                    if longer[j:j+i] != shorter[:i]:
+                        break
+                    elif j == len(longer)-i:
+                        pattern = shorter[:i]
 
         return pattern
