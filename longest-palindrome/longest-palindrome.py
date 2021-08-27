d8449279e631a30aba76f92de8febd6c1174f6a1
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-08-27 13:25:07 -0700

    Update longest-palindrome.py

--- longest-palindrome/longest-palindrome.py
+++ longest-palindrome/longest-palindrome.py
@@ -1,18 +1,17 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         counts = Counter(s)
-        total = 1
-        seenOdd = False
-        
+        total = 0
+
         for i in counts:
             if counts[i] % 2 == 0:
                 total += counts[i]
             else:
-                seenOdd = True
                 total += counts[i] - 1
                 
-        if not seenOdd:
-            total -= 1
+        for i in counts:
+            if counts[i] % 2 == 1:
+                total += 1
+                break
             
         return total
-        
