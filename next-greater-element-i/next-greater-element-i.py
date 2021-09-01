6440e9dcc3029aa426feef5a20b01e447d0b7f76
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-08-31 21:22:54 -0700

    Update next-greater-element-i.py

--- next-greater-element-i/next-greater-element-i.py
+++ next-greater-element-i/next-greater-element-i.py
@@ -2,14 +2,13 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         d = {}
         st = []
-        ans = []
         
         for x in nums2:
             while len(st) and st[-1] < x: # when you run into an upcoming value that is larger than all those in the stack
                 d[st.pop()] = x # set all numbers in stack to the upcoming value, x
             st.append(x) # keep adding all numbers to the stack
 
-        for x in nums1:
-            ans.append(d.get(x, -1))
+        for x in range(len(nums1)):
+            nums1[x] = d.get(nums1[x], -1)
             
-        return ans
+        return nums1
