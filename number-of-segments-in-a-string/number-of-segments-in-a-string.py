3a055d3282adcf2adf171830612a1ddd87f1401c
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-09-02 15:42:15 -0700

    Time: 36 ms (16.27%), Space: 14.1 MB (88.90%) - LeetHub

---
+++ number-of-segments-in-a-string/number-of-segments-in-a-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countSegments(self, s: str) -> int:
+        count = 0
+        
+        for i in range(len(s)):
+            if (i == 0 or s[i-1] == ' ') and s[i] != ' ':
+                count += 1
+              
+        return count
