02f6f5fe357ff77393d7bb589fb04d7b98521247
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-09-07 22:05:49 -0700

    Removed additional addition

--- distance-between-bus-stops/distance-between-bus-stops.py
+++ distance-between-bus-stops/distance-between-bus-stops.py
@@ -2,6 +2,6 @@ class Solution:
     def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
 
         if destination > start:
-            return min(sum(distance[destination:len(distance)]) + sum(distance[0:start]), sum(distance[start:destination]))
+            return min(sum(distance[destination:len(distance)] + distance[0:start]), sum(distance[start:destination]))
         else:
-            return min(sum(distance[start:len(distance)]) + sum(distance[0:destination]), sum(distance[destination:start]))
+            return min(sum(distance[start:len(distance)] + distance[0:destination]), sum(distance[destination:start]))
