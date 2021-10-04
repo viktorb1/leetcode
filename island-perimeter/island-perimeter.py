71b0d800c1a6d9267e1104e0a3b94b6d9585e720
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-10-04 14:47:52 -0700

    Update island-perimeter.py

--- island-perimeter/island-perimeter.py
+++ island-perimeter/island-perimeter.py
@@ -5,14 +5,12 @@ class Solution:
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j] == 1:
-                    if j == 0 or grid[i][j-1] == 0:
-                        total += 1
-                    if j == len(grid[i]) - 1 or grid[i][j+1] == 0:
-                        total += 1
-                    if i == 0 or grid[i-1][j] == 0:
-                        total += 1
-                    if i == len(grid) - 1 or grid[i+1][j] == 0:
-                        total += 1
+                    total += 4
+                    
+                    if j > 0 and grid[i][j-1] == 1:
+                        total -= 2
+                    
+                    if i > 0 and grid[i-1][j] == 1:
+                        total -= 2
         
         return total
-                
