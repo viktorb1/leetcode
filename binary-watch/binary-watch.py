4bcde28cbd5f69037aa826a12bdfe66fbf66459a
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-10-07 18:47:41 -0700

    Time: 28 ms (92.70%), Space: 14.2 MB (84.80%) - LeetHub

---
+++ binary-watch/binary-watch.py
@@ -0,0 +1,23 @@
+from itertools import combinations
+
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        poss = [80, 40, 20, 10, 32, 16, 8, 4, 2, 1]
+        sol = []
+        print(list(combinations(poss, turnedOn)))
+        
+        for i in combinations(poss, turnedOn):
+                    
+            hour = 0
+            minute = 0
+            
+            for j in i:
+                if j % 10 == 0:
+                    hour += j // 10
+                else:
+                    minute += j
+                
+            if hour < 12 and minute < 60:
+                sol.append(f'{hour}:{minute:02}')
+        
+        return sol
