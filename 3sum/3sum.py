2f97d0a96bfab32100294a39ed1e7466e0183090
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-10-16 20:25:07 -0700

    Time: 4473 ms (16.90%), Space: 17.5 MB (73.87%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -4,24 +4,20 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         ans = set()
         nums.sort()
-        counts = Counter(nums)
         
-        
-        for i in range(len(nums)):
-            first = nums[i]
-            counts[first] -= 1
+        for i in range(len(nums) - 2):
+            j = i + 1
+            k = len(nums) - 1
             
-            for j in range(i+1, len(nums)):
-                second = nums[j]
-                counts[second] -= 1
-                third = -(first + second)
-                
-                if third in counts and counts[third] > 0:
-                    ans.add(tuple(sorted((first, second, third))))
-                
-                counts[second] += 1
-                 
-            counts[first] += 1
+            while j < k:
+                curr_sum = nums[i] + nums[j] + nums[k]
 
-        
+                if curr_sum == 0:
+                    ans.add((nums[i], nums[j], nums[k]))
+                    j += 1
+                elif curr_sum < 0:
+                    j += 1
+                else:
+                    k -= 1
+
         return ans
