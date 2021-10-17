97a0bd646a8d9119b2e45300993fb2835e4957cc
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-10-16 20:54:43 -0700

    Update 3sum.py

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -4,12 +4,16 @@ class Solution:
         nums.sort()
         
         for i in range(len(nums) - 2):
+            
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
             j = i + 1
             k = len(nums) - 1
             
             while j < k:
                 curr_sum = nums[i] + nums[j] + nums[k]
-
+                
                 if curr_sum == 0:
                     ans.add((nums[i], nums[j], nums[k]))
                     j += 1
