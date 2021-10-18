4ec8709d5f1d3b2b5656d2b745b16f2953855525
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-10-17 22:13:02 -0700

    Time: 2553 ms (13.13%), Space: 19.3 MB (16.48%) - LeetHub

---
+++ coin-change/coin-change.py
@@ -0,0 +1,30 @@
+class Solution:
+    
+    
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        memo = {}
+        x = self.coinChangeHelper(coins, amount, memo)
+        memo.clear()
+        return x
+
+    def coinChangeHelper(self, coins: List[int], amount: int, memo: Dict) -> int:
+        if amount == 0:
+            return 0
+        
+        minCoins = float('inf') 
+        
+        if amount in memo:
+            return memo[amount]
+        else:
+            for coin in coins:
+                if coin <= amount:
+                    cal = self.coinChangeHelper(coins, amount - coin, memo)
+
+                    if cal != -1:
+                        minCoins = min(minCoins, cal + 1)
+        
+        
+        memo[amount] = minCoins
+        return -1 if minCoins == float('inf') else minCoins
+    
+    
