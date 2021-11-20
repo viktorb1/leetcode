134c95e8a02732af05659017d6e25beb255910b5
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-11-19 16:02:09 -0800

    Time: 100 ms (60.53%), Space: 17.8 MB (11.01%) - LeetHub

---
+++ course-schedule/course-schedule.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        d = defaultdict(set)
+
+        for c, p in prerequisites:
+            d[c].add(p)
+
+        status = defaultdict(int)
+                   
+        def checkpath(c):
+            status[c] = -1
+
+            if c not in d:
+                status[c] = 1
+                return True
+
+            for p in d[c]:
+                if status[p] == 1:
+                    continue
+
+                if status[p] == -1 or not checkpath(p):
+                    return False
+            
+            status[c] = 1
+            return True
+        
+        
+        for c in d:
+            if not checkpath(c):
+                return False
+       
+        return True
