b8ffdba67af416a111a36b205dd2bd8d323a383f
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-11-23 14:55:52 -0800

    Time: 192 ms (11.63%), Space: 14.3 MB (53.58%) - LeetHub

--- combination-sum/combination-sum.py
+++ combination-sum/combination-sum.py
@@ -1,21 +1,16 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        ans = []
-        self.dfs(ans, candidates, target, [])
-        return ans
         
-    
-    def dfs(self, ans, candidates, target, sol):
-        
-        if target == 0 and sorted(sol) not in ans:
-            ans.append(sorted(sol))
-        elif target < 0:
-            return
-        else:
-            for c in candidates:
-                self.dfs(ans, candidates, target - c, sol + [c])
-                
-    
-    
+        def dfs(ans, candidates, target, sol):
             
+            if target == 0 and sorted(sol) not in ans:
+                ans.append(sorted(sol))
+                return
+
+            for c in candidates:
+                if target >= c:
+                    dfs(ans, candidates, target - c, sol + [c])
         
+        ans = []
+        dfs(ans, candidates, target, [])
+        return ans
