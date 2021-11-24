3cc4a24c977685ab3f920514358042239b34496f
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-11-23 20:47:45 -0800

    Update maximal-square.py

--- maximal-square/maximal-square.py
+++ maximal-square/maximal-square.py
@@ -2,8 +2,8 @@ class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         sol = 0
         
-        for y, row in enumerate(matrix):
-            for x, item in enumerate(row):
+        for x, row in enumerate(matrix):
+            for y, item in enumerate(row):
                 if item == '1':                  
                     sol = max(self.getlargest(matrix, x, y), sol)
     
@@ -14,17 +14,16 @@ class Solution:
         
         sol = 2
         
-        while x+sol-1 < len(m[0]) and y+sol-1 < len(m):
+        while x+sol-1 < len(m) and y+sol-1 < len(m[0]):
 
             for yy in range(y, y+sol):
-                if m[yy][x+sol-1] == '0':
+                if m[x+sol-1][yy] == '0':
                     return sol-1
 
             for xx in range(x, x+sol):
-                if m[y+sol-1][xx] == '0':
+                if m[xx][y+sol-1] == '0':
                     return sol-1
         
             sol += 1
             
         return sol-1
-    
