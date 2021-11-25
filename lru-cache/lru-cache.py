42919ef2d575d7ba39fd0173fc4e57c669b6b6e3
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-11-24 16:20:35 -0800

    Time: 6780 ms (5.00%), Space: 76.8 MB (6.40%) - LeetHub

---
+++ lru-cache/lru-cache.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.d = dict()
+        self.q = deque()
+        self.size = 0
+        self.capacity = capacity
+    
+    
+    def get(self, key: int) -> int:
+        if key in self.d:
+            self.q.remove(self.d[key]) # bottleneck
+            self.q.append(self.d[key])
+            return self.d[key][1]
+        
+        return -1
+
+    
+    def put(self, key: int, value: int) -> None:
+        if key in self.d:
+            self.q.remove(self.d[key])
+            self.d[key] = [key, value]
+            self.q.append(self.d[key])
+            return
+        
+        if self.size >= self.capacity:
+            x = self.q.popleft()
+            del self.d[x[0]]
+            self.size -= 1
+        
+        inp = [key, value]
+        self.q.append(inp)
+        self.d[key] = inp
+        self.size += 1
+        
+        
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
