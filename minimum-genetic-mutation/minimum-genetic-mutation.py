4765dab9a396c7420840bcb247d90d5ac221fe47
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-11-27 22:18:33 -0800

    Update minimum-genetic-mutation.py
    cleanup code

--- minimum-genetic-mutation/minimum-genetic-mutation.py
+++ minimum-genetic-mutation/minimum-genetic-mutation.py
@@ -1,24 +1,22 @@
 class Solution:
     def minMutation(self, start: str, end: str, bank: List[str]) -> int:
-        # print(self.hasSingleDifference('AACCGCTA', 'AAACGGTA'))
         return self.dfs(start, end, bank, 0)
         
     
     def dfs(self, cur, end, bank, output):
-        print(cur, end, bank, output)
         if cur == end:
             return output
         
-        ans = float('inf')
+        ans = -1
         
         for idx, b in enumerate(bank):
             if self.hasSingleDifference(cur, b):
                 x = self.dfs(b, end, bank[:idx] + bank[idx+1:], output+1)
                 
                 if x >= 0:
-                    ans = min(x, ans)
-                    
-        return -1 if ans == float('inf') else ans
+                    ans = x if ans == -1 else min(x, ans)
+        
+        return ans
         
         
         
@@ -28,5 +26,5 @@ class Solution:
         for i in range(len(nex)):
             if cur[:i] + nex[i] + cur[i+1:] == nex and cur != nex:
                 count += 1
-        print(count)
+
         return True if count == 1 else False
