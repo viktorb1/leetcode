251744586593751526aa7064cc5180095121c11f
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-11-28 17:53:58 -0800

    Update check-if-one-string-swap-can-make-strings-equal.py

--- check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.py
+++ check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.py
@@ -1,14 +1,11 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        if len(s1) != len(s2):
-            return False
-        elif s1 == s2:
+        if s1 == s2:
             return True
         
-        for i in range(len(s2)):
-            for j in range(i+1, len(s2)):
-                if s1[:i] + s1[j] + s1[i+1:j] + s1[i] + s1[j+1:] == s2:
-                    return True
+        diff = [i for i in range(len(s1)) if s1[i] != s2[i]]
                 
-        return False
-    
+        if len(diff) == 2 and s1[diff[0]] == s2[diff[1]] and s1[diff[1]] == s2[diff[0]]:
+            return True
+        else:
+            return False
