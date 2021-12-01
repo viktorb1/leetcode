150b650eda5f8a270df77b2992bbd87ea2aed03b
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-12-01 15:19:25 -0800

    Update letter-combinations-of-a-phone-number.py

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,17 +1,26 @@
-from itertools import product
+from collections import deque
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        char_map = {
-            '2': 'abc', 
-            '3': 'def', 
-            '4': 'ghi', 
-            '5': 'jkl', 
-            '6': 'mno', 
-            '7': 'pqrs', 
-            '8': 'tuv', 
-            '9': 'wxyz'
-        }
+        char_map = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
         
-        chars = [char_map[i] for i in digits]
-        return [''.join(x) for x in product(*chars)] if digits != "" else []
+        if len(digits) == 0:
+            return []
+        
+        ans = deque()
+        
+        # initially add all letters from first digit (the seed)
+        for l in char_map[digits[0]]:
+            ans.append(l)
+            
+        # loop through the remaining digits
+        for l, d in enumerate(digits[1:], 1):
+           
+            while len(ans[0]) == l:
+                
+                x = ans.popleft()
+
+                for i in char_map[d]:
+                    ans.append(x + i)
+        
+        return ans
