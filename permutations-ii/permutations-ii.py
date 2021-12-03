7408bc24c3be2ed0b1ba71cdd286e475dd6ebd25
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-12-03 01:03:19 -0800

    Update permutations-ii.py

--- permutations-ii/permutations-ii.py
+++ permutations-ii/permutations-ii.py
@@ -1,12 +1,12 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        ans = []
+        ans = set()
         self.choose([], nums, ans)
         return ans
     
     def choose(self, perm, fr, ans):
-        if len(fr) == 0 and perm not in ans:
-            ans.append(perm)
+        if len(fr) == 0:
+            ans.add(tuple(perm))
         else:
             for idx, val in enumerate(fr):
                 self.choose(perm + [val], fr[:idx] + fr[idx+1:], ans)
