be1b6cfe7ee1cac368e73f69ea9d570931b522be
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-12-03 16:11:55 -0800

    Update permutations-ii.py
    Remove duplicate recursive calls

--- permutations-ii/permutations-ii.py
+++ permutations-ii/permutations-ii.py
@@ -1,12 +1,18 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        ans = set()
-        self.choose([], nums, ans)
+        ans = []
+        self.choose([], sorted(nums), ans)
         return ans
     
     def choose(self, perm, fr, ans):
-        if len(fr) == 0:
-            ans.add(tuple(perm))
-        else:
-            for idx, val in enumerate(fr):
-                self.choose(perm + [val], fr[:idx] + fr[idx+1:], ans)
+        if not fr:
+            ans.append(perm)
+        
+        i = 0
+        while i < len(fr):               
+            self.choose(perm + [fr[i]], fr[:i] + fr[i+1:], ans)
+            
+            while i+1 < len(fr) and fr[i] == fr[i+1]:
+                i += 1
+            
+            i += 1
