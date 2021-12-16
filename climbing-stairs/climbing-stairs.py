9720e6242a7c169c4d422991f1e1a42d26b2c989
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-12-16 11:34:15 -0800

    Update climbing-stairs.py

--- climbing-stairs/climbing-stairs.py
+++ climbing-stairs/climbing-stairs.py
@@ -1,17 +1,8 @@
 class Solution:
-    def climbStairsHelper(self, i: int, n: int, memo: List[int]) -> int:
-         
-        if i > n: # we went out of bounds, so it's not a valid sequence
-            return 0
-        elif i == n:
-            return 1 # we stayed in bounds, so it's a valid sequence to climb the stairs
-        elif memo[i] > 0: # previously calculated values, 'memoization'
-            return memo[i]
-        else:
-            memo[i] = self.climbStairsHelper(i+1, n, memo) + self.climbStairsHelper(i+2, n, memo)
-            return memo[i]
-        
     def climbStairs(self, n: int) -> int:
-        memo = [0] * n
-        return self.climbStairsHelper(0, n, memo)
+        n1, n2 = 1, 1
         
+        for i in range(n-1):
+            n1, n2 = n2, n1 + n2
+        
+        return n2
