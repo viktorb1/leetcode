18cf8ee216e790392d1d2612f265cfd7e1e9ebea
Victor Babchanik <vbabchanik@ucdavis.edu>
2021-12-30 17:01:33 -0800

    Update missing-number.py

--- missing-number/missing-number.py
+++ missing-number/missing-number.py
@@ -1,11 +1,4 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        
-        seen = {}
-        
-        for i in nums:
-            seen[i] = 1
-        
-        for i in range(len(nums) + 1):
-            if not i in seen:
-                return i
+        n = len(nums)
+        return n*(n+1) // 2 - sum(nums)
