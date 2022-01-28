dad58f0b10a700b28eadaf00c899a8f358090083
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-01-28 15:41:28 -0800

    Time: 370 ms (49.57%), Space: 28.8 MB (59.43%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
@@ -0,0 +1,42 @@
+class WordDictionary:
+    class PrefixTree():
+        def __init__(self):
+            self.next = dict()
+            self.isWord = False
+    
+    def __init__(self):
+        self.tree = self.PrefixTree()
+
+    def addWord(self, word: str) -> None:
+        cur = self.tree
+    
+        for c in word:
+            if c not in cur.next:
+                cur.next[c] = self.PrefixTree()
+
+            cur = cur.next[c]
+            
+        cur.isWord = True
+                
+    def search(self, word: str, d = None) -> bool:
+        cur = self.tree if d == None else d
+        
+        for i, c in enumerate(word):
+            if c == '.':
+                for key, tre in cur.next.items():
+                    if self.search(word[i+1:], tre):
+                        return True
+                
+                return False
+            elif c not in cur.next:
+                return False
+            else:
+                cur = cur.next[c]
+        
+        return True if cur.isWord else False
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
