924849dc6c33aa51d9fa2ec021d6d2d84207c895
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-01-28 13:13:06 -0800

    Time: 50 ms (43.25%), Space: 14.3 MB (88.05%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -2,7 +2,7 @@ class Solution:
     
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
-        def wordBreakHelper(s, i=0):
+        def wordBreakHelper(i):
             if i == len(s):
                 return True
             elif i in memo:
@@ -10,11 +10,11 @@ class Solution:
 
             for word in wordDict:
                 if word == s[i:i+len(word)]:
-                    memo[i] = wordBreakHelper(s, i+len(word))
+                    memo[i] = wordBreakHelper(i+len(word))
                     if memo[i]:
                         return True
 
             return False
         
         memo = {}
-        return wordBreakHelper(s)
+        return wordBreakHelper(0)
