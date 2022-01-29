f7f99f7c15b747791af4cef817da3b18884b4b11
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-01-28 23:35:14 -0800

    Time: 82 ms (18.36%), Space: 13.9 MB (99.49%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.py
+++ 1025-divisor-game/1025-divisor-game.py
@@ -1,10 +1,3 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
-        dp = [False] * (n+1)
-        
-        for m in range(1, n+1):
-            for i in range(1, ceil(sqrt(m))):
-                if m % i == 0 and dp[m - i] == False:
-                    dp[m] = True
-            
-        return dp[n]
+        return n % 2 == 0
