4153a28260d2c5f85dfa30f327fe1cea40fdd9ad
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-01-29 20:36:29 -0800

    Time: 449 ms (22.88%), Space: 25.3 MB (99.19%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         n = len(nums)
-        k = k %  n
+        k = k % n
 
         self.reverse(nums, 0, n-1)
         self.reverse(nums, 0, k-1)
