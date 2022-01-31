1166d1e7142e7cea52b1692997cdff36f7b0a3b9
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-01-31 12:42:09 -0800

    Time: 43 ms (43.55%), Space: 14.6 MB (16.38%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,11 +1,13 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         dic = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26'}
+        memo = {}
         
-        @lru_cache(cache)
         def helper(s):
             if not s:
                 return 1
+            elif s in memo:
+                return memo[s]
             
             total = 0
             
@@ -13,8 +15,9 @@ class Solution:
                 total += helper(s[1:])
             
             if len(s) > 1 and s[0:2] in dic:
-                total += helper(s[2:])                    
-            
+                total += helper(s[2:])
+                
+            memo[s] = total
             return total
         
         return helper(s)
