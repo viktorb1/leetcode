7f4d69dfd062f4c20b2eba0be93f1f56610d64e9
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-01 14:25:28 -0800

    Time: 44 ms (38.54%), Space: 13.9 MB (98.32%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,9 +1,14 @@
 class Solution:
-    @lru_cache(cache)
+    def __init__(self):
+        self.memo = {}
+    
     def uniquePaths(self, m: int, n: int) -> int:
         if m == 1 and n == 1:
             return 1
         elif m < 1 or n < 1:
             return 0
+        elif (m, n) in self.memo:
+            return self.memo[(m,n)]
         
-        return self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
+        self.memo[(m, n)] = self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
+        return self.memo[(m, n)]
