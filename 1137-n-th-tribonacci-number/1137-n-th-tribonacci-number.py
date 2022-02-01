8d158f4d1588f519a260ea810d5177cac4a6a3cb
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-01-31 21:54:48 -0800

    Time: 32 ms (70.70%), Space: 13.9 MB (98.06%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -2,8 +2,6 @@ class Solution:
     def tribonacci(self, n: int) -> int:
         if n == 0:
             return 0
-        elif n == 1 or n == 2:
-            return 1
         
         first = 0
         second = 1
