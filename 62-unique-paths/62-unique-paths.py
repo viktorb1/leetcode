aee56ec87337628d3acaea37b1150253522cbb79
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-01 15:28:03 -0800

    Time: 45 ms (36.60%), Space: 13.9 MB (94.58%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,14 +1,14 @@
 class Solution:
-    def __init__(self):
-        self.memo = {}
-    
     def uniquePaths(self, m: int, n: int) -> int:
-        if m == 1 and n == 1:
-            return 1
-        elif m < 1 or n < 1:
-            return 0
-        elif (m, n) in self.memo:
-            return self.memo[(m,n)]
+        dp = [[0]*(n) for _ in range(m)] 
         
-        self.memo[(m, n)] = self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
-        return self.memo[(m, n)]
+        dp[0] = [1] * len(dp[0])
+        
+        for i in range(m):
+            dp[i][0] = 1
+                
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+                
+        return dp[-1][-1]
