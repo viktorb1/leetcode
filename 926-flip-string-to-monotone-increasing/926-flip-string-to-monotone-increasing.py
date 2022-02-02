1259cc3ba8636673214acabbbd80e57818e9788d
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-02 10:36:10 -0800

    Time: 384 ms (45.21%), Space: 14.9 MB (94.03%) - LeetHub

---
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minFlipsMonoIncr(self, s: str) -> int:
+        right_zeros = s.count('0')
+        left_ones = 0
+        res = left_ones + right_zeros
+        
+        for c in s:
+            if c == '0':
+                right_zeros -= 1
+            
+            if c == '1':
+                left_ones += 1
+            
+            res = min(res, left_ones + right_zeros)
+
+        
+        return res
+            
