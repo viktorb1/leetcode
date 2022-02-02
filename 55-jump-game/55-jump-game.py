63543ed629e12d1cb27b30fbf299cc48854bb33e
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-02 11:26:44 -0800

    Time: 3130 ms (13.15%), Space: 15.1 MB (91.35%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -6,7 +6,7 @@ class Solution:
         dp[-1] = True
         
         for i in range(len(nums)-2, -1, -1):
-                dp[i] = any(dp[i:i+nums[i]+1])
+            dp[i] = any(dp[i:i+nums[i]+1])
             
         return dp[0]
         
