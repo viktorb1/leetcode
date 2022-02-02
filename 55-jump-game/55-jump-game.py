5f2e8a7fa43336f97e6335728ac2a7cdc83fe98c
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-02 11:28:43 -0800

    Time: 3037 ms (13.34%), Space: 15.4 MB (36.47%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,6 +1,4 @@
 class Solution:
-    
-    lru_cache(None)
     def canJump(self, nums: List[int]) -> bool:
         dp = [False] * len(nums)
         dp[-1] = True
@@ -9,19 +7,3 @@ class Solution:
             dp[i] = any(dp[i:i+nums[i]+1])
             
         return dp[0]
-        
-        
-#         def helper(i):
-#             if i == len(nums) - 1:
-#                 return True
-            
-#             ans = False
-            
-#             for j in range(i+1, min(i+nums[i]+1, len(nums))):
-#                 ans |= helper(j)
-            
-#             return ans
-            
-
-#         return helper(0)
-            
