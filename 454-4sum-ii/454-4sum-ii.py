6251730dc6ae8f6a905fa4a4600bff8d308cc675
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-03 13:58:13 -0800

    Time: 564 ms (99.45%), Space: 14.1 MB (98.22%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.py
+++ 454-4sum-ii/454-4sum-ii.py
@@ -1,18 +1,14 @@
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        last = dict()
+        last = defaultdict(int)
         count = 0
         
         for n3 in nums3:
             for n4 in nums4:
-                if n3+n4 in last:
-                    last[n3+n4] += 1
-                else:
-                    last[n3+n4] = 1
+                last[n3+n4] += 1
             
         for one in nums1:
             for two in nums2:
-                if -(one + two) in last:
-                    count += last[-(one + two)]
+                count += last[-(one + two)]
                         
         return count
