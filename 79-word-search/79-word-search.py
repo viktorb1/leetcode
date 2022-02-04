2df7808f6ddcc052a7318d4df9ee475697cf5dfa
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-03 23:30:03 -0800

    Time: 9547 ms (5.68%), Space: 14 MB (92.35%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,23 +1,25 @@
 class Solution:
-    def exist(self, board, word):
-        if not board:
-            return False
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        def search_letter(w, i, j):
+            if not w:
+                return True
+            if not isInBounds(i, j) or board[i][j] != w[0]:
+                return False
+
+            z = False
+            board[i][j] = "#"
+            
+            for x, y in ((i-1, j), (i, j-1), (i, j+1), (i+1, j)):
+                z |= search_letter(w[1:], x, y)
+            
+            board[i][j] = w[0]
+            return z
+            
+        def isInBounds(i, j):
+            return i >= 0 and i < len(board) and j >= 0 and j < len(board[0])
+        
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if self.dfs(board, i, j, word):
-                    return True
-        return False
-
-    # check whether can find word, start at (i,j) position    
-    def dfs(self, board, i, j, word):
-        if len(word) == 0: # all the characters are checked
-            return True
-        if i<0 or i>=len(board) or j<0 or j>=len(board[0]) or word[0]!=board[i][j]:
-            return False
-        tmp = board[i][j]  # first character is found, check the remaining part
-        board[i][j] = "#"  # avoid visit agian 
-        # check whether can find "word" along one direction
-        res = self.dfs(board, i+1, j, word[1:]) or self.dfs(board, i-1, j, word[1:]) \
-        or self.dfs(board, i, j+1, word[1:]) or self.dfs(board, i, j-1, word[1:])
-        board[i][j] = tmp
-        return res
+                if board[i][j] == word[0]:
+                    if search_letter(word, i, j):
+                        return True
