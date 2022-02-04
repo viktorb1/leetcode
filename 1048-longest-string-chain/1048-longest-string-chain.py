b87b88a8a91f18d0c4e54ed5495cbe2b3de650a9
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-04 12:40:22 -0800

    Time: 132 ms (90.51%), Space: 17.9 MB (9.01%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -3,16 +3,12 @@ class Solution:
         s = set(words)
         
         @cache
-        def dfs(w):
-            l = []
-            for i in range(len(w)):
-                l.append(w[:i] + w[i+1:])
-            
+        def dfs(w):           
             m_val = 0
             
-            for p in l:
-                if p in s:
-                    m_val = max(m_val, 1 + dfs(p))
+            for i in range(len(w)):
+                if w[:i] + w[i+1:] in s:
+                    m_val = max(m_val, 1 + dfs(w[:i] + w[i+1:]))
                     
             return m_val
         
