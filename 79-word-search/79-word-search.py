e77d25243131cf1e952c44f9f61da32e15b66b8a
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-04 00:42:13 -0800

    Time: 9805 ms (5.01%), Space: 14 MB (92.35%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,18 +1,18 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def search_letter(w, i, j):
-            if not w:
+        def search_letter(i, j, n):
+            if n == len(word):
                 return True
-            if not isInBounds(i, j) or board[i][j] != w[0]:
+            if not isInBounds(i, j) or board[i][j] != word[n]:
                 return False
 
             board[i][j] = "#"
             
             for x, y in ((i, j+1), (i+1, j), (i-1, j), (i, j-1)):
-                if search_letter(w[1:], x, y):
+                if search_letter(x, y, n+1):
                     return True
             
-            board[i][j] = w[0]
+            board[i][j] = word[n]
             return False
             
         def isInBounds(i, j):
@@ -20,5 +20,5 @@ class Solution:
         
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if search_letter(word, i, j):
+                if search_letter(i, j, 0):
                     return True
