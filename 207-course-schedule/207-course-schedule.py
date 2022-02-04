791209c821c383fe18c140b8c18daa2546b1dc77
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-03 17:17:14 -0800

    Time: 100 ms (75.73%), Space: 18.8 MB (5.79%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        d = defaultdict(set)
+        seen = set()
+
+        for c, p in prerequisites:
+            d[c].add(p)
+            
+        @cache
+        def checkpath(c):
+            seen.add(c)
+            
+            if c not in d:
+                seen.remove(c)
+                return True
+
+            for p in d[c]:
+                if p in seen or not checkpath(p):
+                    return False
+            
+            seen.remove(c)
+            return True
+        
+        return all(checkpath(c) for c in d)
