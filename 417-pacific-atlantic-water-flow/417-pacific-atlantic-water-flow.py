636472d67ed1f42f69798994beec48d8953dad4a
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-04 18:03:24 -0800

    Time: 3702 ms (5.03%), Space: 23.8 MB (5.06%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,19 +1,19 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         sol = []
-        def reachOcean(i, j, oceans):
-            if i == 0 or j == 0:
-                oceans[0] = True
-            if i == len(heights)-1 or j == len(heights[0])-1:
-                oceans[1] = True
-            if oceans[0] and oceans[1]:
+        memo = {}
+        
+        def reachOcean(i, j, coord):
+            if i == coord[0] or j == coord[1]:
                 return True
+            if (i, j, coord) in memo:
+                return memo[(i, j, coord)]
 
             tmp = heights[i][j]
             heights[i][j] = float('inf')
             
             for x, y in ((i+1, j), (i, j+1), (i-1, j), (i, j-1)):
-                if isInRange(x, y) and tmp >= heights[x][y] and reachOcean(x, y, oceans):
+                if isInRange(x, y) and tmp >= heights[x][y] and reachOcean(x, y, coord):
                     heights[i][j] = tmp
                     return True
             
@@ -25,9 +25,10 @@ class Solution:
         
         for i in range(len(heights)):
             for j in range(len(heights[0])):
-                if reachOcean(i,j, [False, False]):
+                memo[i, j, (0, 0)] = reachOcean(i, j, (0, 0))
+                memo[i, j, (len(heights)-1, len(heights[0])-1)] = reachOcean(i, j, (len(heights)-1, len(heights[0])-1))
+                
+                if memo[i, j, (0, 0)] and memo[i, j, (len(heights)-1, len(heights[0])-1)]:
                     sol.append((i, j))
-                    
+        
         return sol
-                    
-                    
