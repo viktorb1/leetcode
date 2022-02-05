3348be62361d040d5b95881f4499fd0fb1b9cbad
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-04 17:24:06 -0800

    Time: 5017 ms (5.03%), Space: 15.6 MB (57.17%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,15 +1,19 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         sol = []
-        def reachOcean(i, j, coord):
-            if i == coord[0] or j == coord[1]:
+        def reachOcean(i, j, oceans):
+            if i == 0 or j == 0:
+                oceans[0] = True
+            if i == len(heights)-1 or j == len(heights[0])-1:
+                oceans[1] = True
+            if oceans[0] and oceans[1]:
                 return True
 
             tmp = heights[i][j]
             heights[i][j] = float('inf')
             
             for x, y in ((i+1, j), (i, j+1), (i-1, j), (i, j-1)):
-                if isInRange(x, y) and tmp >= heights[x][y] and reachOcean(x, y, coord):
+                if isInRange(x, y) and tmp >= heights[x][y] and reachOcean(x, y, oceans):
                     heights[i][j] = tmp
                     return True
             
@@ -21,7 +25,7 @@ class Solution:
         
         for i in range(len(heights)):
             for j in range(len(heights[0])):
-                if reachOcean(i, j, (0, 0)) and reachOcean(i, j, (len(heights)-1, len(heights[0])-1)):
+                if reachOcean(i,j, [False, False]):
                     sol.append((i, j))
                     
         return sol
