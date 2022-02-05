c572df332fb037f1891e0634841acb77035cd436
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-04 16:49:21 -0800

    Time: 3084 ms (5.82%), Space: 15.5 MB (57.17%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,8 +1,8 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         sol = []
-        def reachPacific(i, j):
-            if i == 0 or j == 0:
+        def reachOcean(i, j, coord):
+            if i == coord[0] or j == coord[1]:
                 return True
 
             tmp = heights[i][j]
@@ -10,36 +10,19 @@ class Solution:
             
             
             for x, y in ((i+1, j), (i, j+1), (i-1, j), (i, j-1)):
-                if isInRange(x, y) and tmp >= heights[x][y] and reachPacific(x, y):
+                if isInRange(x, y) and tmp >= heights[x][y] and reachOcean(x, y, coord):
                     heights[i][j] = tmp
                     return True
             
             heights[i][j] = tmp
             return False
-        
-        
-        def reachAtlantic(i, j):
-            if i == len(heights) - 1 or j == len(heights[0]) - 1:
-                return True
-
-            tmp = heights[i][j]
-            heights[i][j] = float('inf')
-            
-            for x, y in ((i+1, j), (i, j+1), (i-1, j), (i, j-1)):
-                if isInRange(x, y) and tmp >= heights[x][y] and reachAtlantic(x, y):
-                    heights[i][j] = tmp
-                    return True
-            
-            heights[i][j] = tmp
-            return False
-        
         
         def isInRange(i, j):
             return i >= 0 and i < len(heights) and j >= 0 and j < len(heights[0])
         
         for i in range(len(heights)):
             for j in range(len(heights[0])):
-                if reachAtlantic(i, j) and reachPacific(i, j):
+                if reachOcean(i, j, (0, 0)) and reachOcean(i, j, (len(heights)-1, len(heights[0])-1)):
                     sol.append((i, j))
                     
         return sol
