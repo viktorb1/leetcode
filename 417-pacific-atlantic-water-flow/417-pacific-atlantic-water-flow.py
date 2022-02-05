8af89e8a8f69f173dc92ad549819439ea954c63c
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-04 18:57:55 -0800

    Time: 680 ms (13.45%), Space: 15.9 MB (19.58%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,40 +1,25 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        sol = []
-        memo = {}
+        rows = len(heights)
+        cols = len(heights[0])
+        pacific = set()
+        atlantic = set()
         
-        def reachOcean(i, j, coord):
-            if i == coord[0] or j == coord[1]:
-                return True
-
-            tmp = heights[i][j]
-            heights[i][j] = float('inf')
+        def dfs(i, j, visited, prev):
+            if i < 0 or i >= rows or j < 0 or j >= cols or heights[i][j] < prev or (i, j) in visited:
+                return
+            
+            visited.add((i, j))
             
             for x, y in ((i+1, j), (i, j+1), (i-1, j), (i, j-1)):
-                if isInRange(x, y) and tmp >= heights[x][y] and reachOcean(x, y, coord):
-                    heights[i][j] = tmp
-                    return True
+                dfs(x, y, visited, heights[i][j])
+        
+        for c in range(cols):
+            dfs(0, c, pacific, 0)
+            dfs(rows-1, c, atlantic, 0)
             
-            heights[i][j] = tmp
-            return False
-        
-        def isInRange(i, j):
-            return i >= 0 and i < len(heights) and j >= 0 and j < len(heights[0])
-        
-        for i in range(len(heights)):
-            for j in range(len(heights[0])):
-                x, y = False, False
-                if (i, j, (0, 0)) in memo:
-                    x = memo[i, j, (0, 0)]
-                else:
-                    x = reachOcean(i, j, (0, 0))
-                
-                if (i, j, (len(heights)-1), len(heights[0])-1) in memo:
-                    y = memo[(len(heights)-1, len(heights[0])-1)]
-                else:
-                    y = reachOcean(i, j, (len(heights)-1, len(heights[0])-1))
-                
-                if x and y:
-                    sol.append((i, j))
-        
-        return sol
+        for r in range(rows):
+            dfs(r, 0, pacific, 0)
+            dfs(r, cols-1, atlantic, 0)
+            
+        return pacific.intersection(atlantic)
