defc1909f6f44ba695c7322598b48f649555c506
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-05 00:26:47 -0800

    Time: 158 ms (45.41%), Space: 17.8 MB (79.64%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -8,43 +8,11 @@ class Solution:
         ans = []
         for l in lists:
             while l:
-                ans.append(ListNode(l.val))
+                ans.append(l)
                 l = l.next
                 
-        ans = self.sortListNodes(ans)
-
+        ans.sort(key=lambda x: x.val)
         for i, node in enumerate(ans[:-1]):
             node.next = ans[i+1]
         
         return ans[0] if len(ans) > 0 else None
-    
-    def sortListNodes(self, ans):
-        if len(ans) <= 1:
-            return ans
-        
-        ans1 = self.sortListNodes(ans[:len(ans)//2])
-        ans2 = self.sortListNodes(ans[len(ans)//2:])
-        return self.combine(ans1, ans2)
-            
-
-    def combine(self, ans1, ans2):
-        i, j = 0, 0
-        sol = []
-
-        while i < len(ans1) and j < len(ans2):
-            if ans1[i].val < ans2[j].val:
-                sol.append(ans1[i])
-                i += 1
-            else:
-                sol.append(ans2[j])
-                j += 1
-
-        while i < len(ans1):
-            sol.append(ans1[i])
-            i += 1
-
-        while j < len(ans2):
-            sol.append(ans2[j])
-            j += 1
-        
-        return sol
