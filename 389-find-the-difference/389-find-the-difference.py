3a98fad6cb4ae605db587e2a5562638cecd9059d
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-06 17:16:22 -0800

    Time: 69 ms (12.35%), Space: 13.9 MB (96.95%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,6 +1,11 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        counter = Counter(t) - Counter(s)
+        letter = 0
         
-        for c in counter:
-            return c
+        for c in t:
+            letter += ord(c)
+            
+        for c in s:
+            letter -= ord(c)
+            
+        return chr(letter)
