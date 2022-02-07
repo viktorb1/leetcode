37f6410b6c4edf4bfcb662a83a534bf7aa074331
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-07 11:04:45 -0800

    Time: 521 ms (65.84%), Space: 61.2 MB (94.76%) - LeetHub

---
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
@@ -0,0 +1,38 @@
+import random
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.items = dict()
+        self.mirror = []
+
+    def insert(self, val: int) -> bool:
+        if val in self.items:
+            return False
+        else:
+            self.items[val] = len(self.mirror)
+            self.mirror.append(val)
+            return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.items:
+            return False
+        else:
+            idx_to_del = self.items[val]
+            val_to_rpc = self.mirror.pop()
+            if idx_to_del < len(self.mirror):
+                self.mirror[idx_to_del] = val_to_rpc
+                self.items[val_to_rpc] = idx_to_del
+            del self.items[val]
+            return True
+
+    def getRandom(self) -> int:
+        x = random.randint(0, len(self.mirror)-1)
+        return self.mirror[x]
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
