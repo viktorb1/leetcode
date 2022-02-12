04e7b81a5eae96d954bbc333d8e01bccc3611de1
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-11 22:11:30 -0800

    Time: 278 ms (24.00%), Space: 13.9 MB (91.40%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.py
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.py
@@ -4,11 +4,9 @@ class Solution:
         numtens = 0
         
         for i in range(5, n+1):
-            if i % 10 == 0:
-                numtens += 1
-                i //= 10
             while i % 5 == 0:
                 numfives += 1
-                i //= 5
+                i = i // 5
+                
 
         return numfives + numtens
