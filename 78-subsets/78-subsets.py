ad4c3e0b0930347c17d25c33c792f8c35d7ce0ce
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-13 03:24:51 -0800

    Time: 40 ms (61.13%), Space: 14.2 MB (82.34%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,10 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        output = []
-        n = len(nums)
-        
-        for m in range(2**n, 2**(n+1)):
-            bitmask = bin(m)[3:]
-            output.append([num for i, num in enumerate(nums) if bitmask[i] == '1'])
             
+        def backtrack(nums, curr):
+            if len(curr) == k:
+                output.append(curr[:])
+                return 
+            
+            for idx, num in enumerate(nums):
+                backtrack(nums[idx+1:], curr + [num])
+        
+        output = []
+
+        for k in range(len(nums) + 1):
+            backtrack(nums, [])
+        
         return output
