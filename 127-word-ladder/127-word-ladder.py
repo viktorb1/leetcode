fb4714f5f34cd1244dd061b3ac66bc61aaa8dcb4
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-13 00:15:47 -0800

    Time: 500 ms (39.66%), Space: 15.1 MB (90.52%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -1,14 +1,11 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         seen = set()
-        
+        wordList = set(wordList)
+
         q = deque([(beginWord, 0)])
         ans = float('inf')
         
-        matches = defaultdict(list)
-        for word in wordList:
-            for i in range(len(beginWord)):
-                matches[word[:i] + "*" + word[i+1:]].append(word)
         
         while q:
             beginWord, j = q.popleft()
@@ -19,8 +16,10 @@ class Solution:
             seen.add(beginWord)
             
             for i in range(len(beginWord)):
-                for w in matches[beginWord[:i] + "*" + beginWord[i+1:]]:
-                    if w not in seen:
+                for c in "abcdefghijklmnopqrstuvwxyz":
+                    w = beginWord[:i] + c + beginWord[i+1:]
+                    if w in wordList:
                         q.append((w, j+1))
+                        wordList.discard(w)
         
         return 0
