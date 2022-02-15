f0a2c88348b80c73f2f75f65a862bbf610765073
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-15 00:38:25 -0800

    Time: 174 ms (43.35%), Space: 14.8 MB (73.72%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -7,27 +7,23 @@ class Solution:
         
         l, r = 0, 0
         while r < len(s):
-            if s[r] in ct:
-                ct[s[r]] -= 1
+            ct[s[r]] -= 1
                 
-                if ct[s[r]] == 0:
-                    need.discard(s[r])
+            if ct[s[r]] == 0:
+                need.discard(s[r])
             
             while len(need) == 0:
                 if r-l+1 < minLen:
                     minLen = r-l+1
                     idxstart = l
                 
-                if s[l] in ct:
-                    ct[s[l]] += 1
-                    if ct[s[l]] == 1:
-                        need.add(s[l])
+                ct[s[l]] += 1
+                if ct[s[l]] == 1:
+                    need.add(s[l])
                   
                 l += 1
             
             r += 1
 
-
         return s[idxstart:idxstart+minLen] if minLen != float('inf') else ""
-                
-            
+        
