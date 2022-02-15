0af7e43f61ca331a9fd6fcd07e782c3629fd02ea
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-15 11:27:53 -0800

    Time: 268 ms (24.53%), Space: 14.8 MB (73.72%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,23 +1,25 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        ct = Counter(t)
+        counter_t = Counter(t)
         need = set(t)
+        l = 0
         coord = (float('-inf'), float('inf'))
         
-        l, r = 0, 0
-        while r < len(s):
-            ct[s[r]] -= 1
-            if ct[s[r]] <= 0:
-                need.discard(s[r])
+        for r, c in enumerate(s):
+            counter_t[c] -= 1
             
-            while len(need) == 0:
-                coord = min((l, r + 1), coord, key = lambda x: x[1] - x[0])
+            if counter_t[c] == 0:
+                need.discard(c)
                 
-                ct[s[l]] += 1
-                if ct[s[l]] > 0:
+            while len(need) == 0:
+                coord = min(coord, (l, r), key = lambda x: x[1] - x[0])
+                counter_t[s[l]] += 1
+                
+                if counter_t[s[l]] > 0:
                     need.add(s[l])
+                
                 l += 1
-            r += 1
-
-        return s[coord[0]:coord[1]] if coord[0] != float('-inf') and coord[1] != float('inf') else ""
+                    
         
+        return s[coord[0]:coord[1]+1] if coord[0] != float('-inf') and coord[1] != float('inf') else ""
+            
