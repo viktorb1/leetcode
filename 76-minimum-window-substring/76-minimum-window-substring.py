6ac08f0efed50719c3f85cc926cb17ef4c527406
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-15 00:59:07 -0800

    Time: 180 ms (41.05%), Space: 14.8 MB (49.67%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -2,28 +2,22 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         ct = Counter(t)
         need = set(t)
-        minLen = float('inf')
-        idxstart = 0
+        coord = (float('-inf'), float('inf'))
         
         l, r = 0, 0
         while r < len(s):
             ct[s[r]] -= 1
-                
-            if ct[s[r]] == 0:
+            if ct[s[r]] <= 0:
                 need.discard(s[r])
             
             while len(need) == 0:
-                if r-l+1 < minLen:
-                    minLen = r-l+1
-                    idxstart = l
+                coord = min((l, r + 1), coord, key = lambda x: x[1] - x[0])
                 
                 ct[s[l]] += 1
-                if ct[s[l]] == 1:
+                if ct[s[l]] > 0:
                     need.add(s[l])
-                  
                 l += 1
-            
             r += 1
 
-        return s[idxstart:idxstart+minLen] if minLen != float('inf') else ""
+        return s[coord[0]:coord[1]] if coord[0] != float('-inf') and coord[1] != float('inf') else ""
         
