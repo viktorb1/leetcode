b0135693bbb7019c0d7877ade905db5d5c772800
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-15 16:19:55 -0800

    Time: 224 ms (7.83%), Space: 20 MB (7.80%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        
+        r, c = len(grid), len(grid[0])
         @cache
         def dfs(i, j):
-            if i >= len(grid) or j >= len(grid[0]):
+            if i >= r or j >= c:
                 return float('inf')
-            elif (i, j) == (len(grid) - 1, len(grid[0]) - 1):
+            elif (i, j) == (r - 1, c - 1):
                 return grid[i][j]
             
             return grid[i][j] + min(dfs(i+1, j), dfs(i, j+1))
