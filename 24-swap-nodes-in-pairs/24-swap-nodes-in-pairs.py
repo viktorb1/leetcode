89054a0af24c34601fa5a80359fd27f1e66dc6fc
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-15 18:18:41 -0800

    Time: 41 ms (52.20%), Space: 13.8 MB (90.54%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
@@ -8,13 +8,13 @@ class Solution:
         save_for_return = two
         
         while one and two:
-            save1 = two.next # save 3
-            save2 = two.next.next if two.next else None # save 4
+            three = two.next # save 3
+            four = two.next.next if two.next else None # save 4
             
-            one.next = two.next.next if two.next and two.next.next else two.next # make 1 point to 4
+            one.next = four if three and four else three # make 1 point to 4
             two.next = one # make 2 point to 1
             
-            one = save1 # update 1 to 3
-            two = save2 # update 2 to 4
+            one = three # update 1 to 3
+            two = four # update 2 to 4
             
         return save_for_return
