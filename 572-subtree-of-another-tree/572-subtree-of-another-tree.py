d3f73ac7ac80c5428242d625caad9c5ee84f4db6
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-17 13:54:46 -0800

    Time: 124 ms (90.41%), Space: 15 MB (74.54%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -10,7 +10,7 @@ class Solution:
         if self.checkEquality(root, subRoot):
             return True
         
-        if root and subRoot:
+        if root:
             return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
         else:
             return False
