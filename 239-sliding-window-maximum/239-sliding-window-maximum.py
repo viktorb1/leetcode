d6341aea703f46491a688fb2005e94990c60be73
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-16 16:21:01 -0800

    Time: 2141 ms (63.31%), Space: 31 MB (11.31%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -3,20 +3,20 @@ class Solution:
         l, r = 0, 0
         q = deque()
         sol = []
+        
         while r < len(nums):
             while q and nums[r] > nums[q[-1]]:
                 q.pop()
-
+            
             q.append(r)
             
-            while l > q[0]:
+            if q[0] < l:
                 q.popleft()
-                
-            if r+1-l == k:
+            
+            if r-l+1 == k:
                 sol.append(nums[q[0]])
                 l += 1
             
             r += 1
-                
             
         return sol
