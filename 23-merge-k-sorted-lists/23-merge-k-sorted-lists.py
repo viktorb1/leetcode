56fe7f9199512781e139cb46127d1e4cd23df6b9
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-22 12:42:57 -0800

    Time: 310 ms (13.24%), Space: 18.5 MB (15.39%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -7,27 +7,28 @@ from queue import PriorityQueue
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
-        class Wrapper():
+        class Wrapper:
             def __init__(self, node):
                 self.node = node
+                
             def __lt__(self, other):
                 return self.node.val < other.node.val
             
-        start = ListNode(0)
-        cur = start
         q = PriorityQueue()
+        start = cur = ListNode(0)
         
         for l in lists:
             if l:
                 q.put(Wrapper(l))
+                
             
         while not q.empty():
-            node = q.get().node
-            cur.next = node
+            smallest = q.get().node
+            cur.next = smallest
             cur = cur.next
             
-            if node.next:
-                q.put(Wrapper(node.next))
-
+            if smallest.next:
+                q.put(Wrapper(smallest.next))
+            
+            
         return start.next
