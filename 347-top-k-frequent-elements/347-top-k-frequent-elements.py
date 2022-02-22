041c801801ff9ad0ddaab95e29f886d0907cf9b7
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-22 15:10:54 -0800

    Time: 185 ms (20.90%), Space: 18.8 MB (33.29%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,15 +1,39 @@
-from queue import PriorityQueue
+import random
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = Counter(nums)
-        q = PriorityQueue()
+        vals = [i for i in count]
         
-        for c, v in count.items():
-            q.put((-v, c))
+        
+        def partition(vals, start, end, pivot):
+            vals[pivot], vals[end] = vals[end], vals[pivot]
             
-        sol = []
-        for _ in range(k):
-            sol.append(q.get()[1])
+            dividor = start
+            for i in range(start, end+1):
+                if count[vals[i]] > count[vals[end]]:
+                    vals[dividor], vals[i] = vals[i], vals[dividor]
+                    dividor += 1
             
-        return sol
+            vals[end], vals[dividor] = vals[dividor], vals[end]
+            return dividor
+        
+        
+        def quickselect(start, end):
+            
+            if start == end:
+                return
+            
+            pivot = partition(vals, start, end, random.randint(start, end))
+            
+            if pivot == k:
+                return
+            elif pivot > k:
+                quickselect(start, pivot - 1)
+            elif pivot < k:
+                quickselect(pivot + 1, end)
+        
+        
+        quickselect(0, len(vals)-1)
+        return vals[:k]
+                
