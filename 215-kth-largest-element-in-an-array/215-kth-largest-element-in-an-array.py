0a1bd1e68acdc902e46cbf7c5b6e194f87011afc
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-22 18:18:01 -0800

    Time: 80 ms (68.49%), Space: 15.5 MB (12.25%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -0,0 +1,32 @@
+import random
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        def partition(start, end, pivot):
+            nums[pivot], nums[end] = nums[end], nums[pivot]
+            
+            divider = start
+            for i in range(start, end):
+                if nums[i] > nums[end]:
+                    nums[divider], nums[i] = nums[i], nums[divider]
+                    divider += 1
+            
+            nums[divider], nums[end] = nums[end], nums[divider]
+            return divider
+        
+        def selectKth(start, end):
+            if start == end:
+                return nums[start]
+            
+            x = random.randint(start, end)
+            divider = partition(start, end, x)
+
+            if divider == k - 1:
+                return nums[divider]
+            elif divider < k - 1:
+                return selectKth(divider+1, end)
+            else:
+                return selectKth(start, divider-1)
+            
+        return selectKth(0, len(nums)-1)
+                    
