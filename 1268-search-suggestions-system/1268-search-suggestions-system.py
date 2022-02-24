440868a4659f4db9f2446c1c5e57cadaf2c05625
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-23 23:43:20 -0800

    Time: 1019 ms (14.19%), Space: 22.7 MB (8.27%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,14 +1,56 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+    
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        cur = self.root
+        
+        for c in word:
+            if c in cur.children:
+                cur = cur.children[c]
+            else:
+                cur.children[c] = TrieNode()
+                cur = cur.children[c]
+        cur.isWord = True
+        
+    def getWordsWithPrefix(self, prefix):
+        cur = self.root
+        
+        for c in prefix:
+            if c not in cur.children:
+                return []
+            else:
+                cur = cur.children[c]
+        
+        res = []
+        self.dfs(cur, res, prefix)
+        return res
+    
+    def dfs(self, cur, res, word):
+        if len(res) == 3:
+            return
+        if cur.isWord:
+            res.append(word)
+        
+        for c in sorted(cur.children):
+            self.dfs(cur.children[c], res, word + c)
+        
+        
+
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        products.sort()
-        sol = []
+        trie = Trie()
         
+        for word in products:
+            trie.insert(word)
+        
+        sol = []
         for i in range(1, len(searchWord)+1):
-            for s, p in enumerate(products):
-                if p[:i] == searchWord[:i]:
-                    sol.append([p for p in products[s:s+3] if p[:i] == searchWord[:i]])
-                    break
-            else:
-                sol.append([])
-                    
+            sol.append(trie.getWordsWithPrefix(searchWord[:i]))
         return sol
+        
