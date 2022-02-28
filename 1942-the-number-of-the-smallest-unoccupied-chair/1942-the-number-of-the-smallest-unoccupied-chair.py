d234ad41c51396aab24c9d5cb342ac0d9a460118
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-28 10:11:14 -0800

    Time: 4431 ms (5.08%), Space: 20.5 MB (7.34%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.py
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.py
@@ -12,17 +12,13 @@ class Solution:
         
         while not q.empty():
             _, which, i = q.get()
-            
-            if which == 1:
+            if i == targetFriend:
+                return chair.index(0)
+            elif which == 1:
                 idx = chair.index(0)
-                
-                if i == targetFriend:
-                    return idx
-                
                 chair[idx] = 1
                 picked_chairs[i] = idx
             else:
                 chair[picked_chairs[i]] = 0
-                
         
         return -1
