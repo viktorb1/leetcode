6a7f0ff1317be4f647290efba3e55d7efd6446a7
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-02-28 10:08:54 -0800

    Time: 4300 ms (5.08%), Space: 20.3 MB (19.77%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.py
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.py
@@ -1,21 +1,28 @@
+from queue import PriorityQueue
+
 class Solution:
     def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
+        q = PriorityQueue()
+        for i, (start, end) in enumerate(times):
+            q.put((start, 1, i))
+            q.put((end, 0, i))
         
-        starts = sorted([(start, i) for i, (start, _) in enumerate(times)])
-        ends = sorted([(end, i) for i, (_, end) in enumerate(times)])
-        s, e = 0, 0
         chair = [0] * len(times)
         picked_chairs = {}
         
-        while s < len(starts) and e < len(ends):
-            if ends[e][0] <= starts[s][0]:
-                chair[picked_chairs[ends[e][1]]] = 0
-                e += 1
-            elif starts[s][0] == times[targetFriend][0]:
-                return chair.index(0)
-            elif starts[s][0] < ends[e][0]:
-                picked_chairs[starts[s][1]] = chair.index(0)
-                chair[picked_chairs[starts[s][1]]] = 1
-                s += 1
+        while not q.empty():
+            _, which, i = q.get()
+            
+            if which == 1:
+                idx = chair.index(0)
+                
+                if i == targetFriend:
+                    return idx
+                
+                chair[idx] = 1
+                picked_chairs[i] = idx
+            else:
+                chair[picked_chairs[i]] = 0
+                
         
         return -1
