52f6a09855770c92d01044cdb2a07aa61e013d56
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-03-01 13:52:02 -0800

    Time: 42 ms (65.56%), Space: 14 MB (62.84%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.py
+++ 29-divide-two-integers/29-divide-two-integers.py
@@ -3,15 +3,14 @@ class Solution:
         is_negative = (dividend < 0) != (divisor < 0)
         divisor, dividend = abs(divisor), abs(dividend)
         quotient = 0
-        the_sum = divisor
 
-        while the_sum <= dividend:
+        while divisor <= dividend:
+            the_sum = divisor
             num_quotients_added = 1
-            while (the_sum + the_sum) <= dividend:
+            while the_sum + the_sum <= dividend:
                 the_sum += the_sum
                 num_quotients_added += num_quotients_added
             dividend -= the_sum
-            the_sum = divisor
             quotient += num_quotients_added
 
         if is_negative:
