33e6500bff71a2a2b16b30c8816efdf70b3cf965
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-03-03 10:16:39 -0800

    Time: 80 ms (7.64%), Space: 14.8 MB (5.44%) - LeetHub

---
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        count = 0
+        longest_slices = dict()
+        
+        i = 0
+        while i < len(nums)-1:
+            start = i
+            diff = nums[i+1] - nums[i]
+            
+            while i < len(nums)-1 and nums[i+1] - nums[i] == diff:
+                i += 1
+            
+            longest_slices[(diff, start, i)] = i - start + 1
+        
+        for s in longest_slices:
+            for i in range(3, longest_slices[s] + 1):
+                count += longest_slices[s] - i + 1
+        
+        return count
