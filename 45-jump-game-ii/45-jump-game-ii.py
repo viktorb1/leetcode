142095a491c4e5236dea702a87923951e1c26649
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-03-03 09:53:39 -0800

    Time: 229 ms (50.83%), Space: 15 MB (75.43%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -4,13 +4,13 @@ class Solution:
         l, r = 0, 0
         count = 0
         
-        while r < len(nums)-1:
-            new_r = 0
-
-            for i in range(l, r+1):
-                new_r = max(new_r, i+nums[i])
+        while r < len(nums) - 1:
+            new_r = float('-inf')
             
-            l, r = r+1, new_r
+            for i in range(l, r+1):
+                new_r = max(new_r, i + nums[i])
+            
+            l, r = r, new_r
             count += 1
         
         return count
