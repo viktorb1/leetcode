3eceb71b80fcd8f5cfbd3051a1ab4db31a6c5237
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-03-03 21:31:09 -0800

    Time: 160 ms (57.77%), Space: 14 MB (67.48%) - LeetHub

---
+++ 799-champagne-tower/799-champagne-tower.py
@@ -0,0 +1,15 @@
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        
+        tower = [[0] * k for k in range(1, 102)]
+        tower[0][0] = poured
+        
+        for r in range(query_row):
+            for c in range(r+1):
+                q = (tower[r][c] - 1.0) / 2.0
+                
+                if q > 0:
+                    tower[r+1][c] += q
+                    tower[r+1][c+1] += q
+                    
+        return min(1, tower[query_row][query_glass])
