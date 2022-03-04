18b34816822501700f3087af3722449e16769095
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-03-04 00:00:06 -0800

    Time: 40 ms (60.53%), Space: 13.8 MB (80.59%) - LeetHub

---
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -0,0 +1,12 @@
+class Solution:
+    @cache
+    def numTrees(self, n: int) -> int:
+        if n <= 1:
+            return 1
+        
+        count = 0
+        for i in range(1, n+1):
+            count += self.numTrees(i-1) * self.numTrees(n-i)
+        return count
+            
+        
