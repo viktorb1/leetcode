f8634e4b62a0dc161455408c33bc819fe715fa6b
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-03-04 14:07:50 -0800

    Time: 32 ms (86.01%), Space: 13.9 MB (77.54%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.py
+++ 856-score-of-parentheses/856-score-of-parentheses.py
@@ -1,18 +1,15 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
-        stack = []
         points = 1
         total = 0
         
         for i, c in enumerate(s):
             if c == '(':
-                stack.append(points)
                 points *= 2
             elif s[i-1:i+1] == '()':
-                total += stack.pop()
+                total += points // 2
                 points //= 2
             elif c == ')':
-                stack.pop()
                 points //= 2
         
         return total
