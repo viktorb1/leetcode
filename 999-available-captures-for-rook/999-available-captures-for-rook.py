34840675464a13f2d2a9537e9684a30ed9de8478
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-03-03 20:14:18 -0800

    Time: 58 ms (18.87%), Space: 13.9 MB (64.96%) - LeetHub

---
+++ 999-available-captures-for-rook/999-available-captures-for-rook.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numRookCaptures(self, board: List[List[str]]) -> int:
+        for i in range(len(board)):
+            for j in range((len(board[0]))):
+                if board[i][j] == 'R':
+                    count = 0
+                    k,l = i-1,j-1
+                    m,n = i+1,j+1
+                    
+                    while k > 0 and board[k][j] == '.':
+                        k -= 1
+                    
+                    while l > 0 and board[i][l] == '.':
+                        l -= 1
+                    
+                    while m < 7 and board[m][j] == '.':
+                        m += 1
+                    
+                    while n < 7 and board[i][n] == '.':
+                        n += 1
+                    
+                    potential_matches = (
+                        board[k][j],
+                        board[i][l],
+                        board[m][j],
+                        board[i][n]
+                    )
+                    
+                    count = sum(a == 'p' for a in potential_matches)
+                    return count
