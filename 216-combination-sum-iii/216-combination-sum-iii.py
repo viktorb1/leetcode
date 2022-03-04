3c0c9f5e40b86130e3ba41a799fe82d2f99fd350
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-03-04 14:26:11 -0800

    Time: 39 ms (67.82%), Space: 13.9 MB (88.92%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        choose = [i+1 for i in range(min(n, 9))]
+        sol = []
+        
+        def dfs(choose, nums, total):
+            if total == n and len(nums) == k:
+                sol.append(nums)
+                print(sol)
+                return
+
+            for i, num in enumerate(choose):
+                if total + num <= n and len(nums) < k:
+                    dfs(choose[i+1:], nums + [num], total + num)
+        
+        dfs(choose, [], 0)
+        return sol
