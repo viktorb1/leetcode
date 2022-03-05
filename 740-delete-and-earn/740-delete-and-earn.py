8e5bbfca0028e5b73c4ebd47da1c0a2c0e143bf1
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-03-04 21:07:13 -0800

    Time: 164 ms (12.66%), Space: 26.8 MB (5.02%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -1,22 +1,20 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        d = defaultdict(int)
+        d = Counter(nums)
+        keys = tuple(set(nums))
         nums.sort()
         
-        for n in nums:
-            d[n] += n
-         
         @cache
         def dfs(keys):
             if not keys:
                 return 0
             elif len(keys) == 1:
-                return d[keys[0]]
+                return keys[0]*d[keys[0]]
             
             if keys[-2] != keys[-1] - 1:
-                return d[keys[-1]] + dfs(keys[:-1])
+                return keys[-1]*d[keys[-1]] + dfs(keys[:-1])
             else:
-                return max(d[keys[-1]] + dfs(keys[:-2]), dfs(keys[:-1]))
+                return max(keys[-1]*d[keys[-1]] + dfs(keys[:-2]), dfs(keys[:-1]))
         
-        return dfs(tuple(d.keys()))
+        return dfs(keys)
         
