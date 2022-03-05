c33c924c36cef8e6f9791ebfa329fa21aafb8a4f
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-03-04 21:42:01 -0800

    Time: 70 ms (70.58%), Space: 14.1 MB (85.04%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -2,8 +2,8 @@ class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         vals = sorted(set(nums))
         c = Counter(nums)
-        count = 0
         sum1, sum2 = 0, 0
+        
         for i, v in enumerate(vals):
             total = v*c[v]
             if vals[i-1] == vals[i] - 1:
