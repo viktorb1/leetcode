a0791b8fa18a1e45b616c81d9c8d0ef4b4d620d7
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-03-04 21:43:56 -0800

    Time: 48 ms (99.06%), Space: 14.2 MB (85.04%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -5,10 +5,9 @@ class Solution:
         sum1, sum2 = 0, 0
         
         for i, v in enumerate(vals):
-            total = v*c[v]
             if vals[i-1] == vals[i] - 1:
-                sum1, sum2 = sum2, max(total + sum1, sum2)
+                sum1, sum2 = sum2, max(v*c[v] + sum1, sum2)
             else:
-                sum1, sum2 = sum2, sum2 + total
+                sum1, sum2 = sum2, sum2 + v*c[v]
         
         return sum2
