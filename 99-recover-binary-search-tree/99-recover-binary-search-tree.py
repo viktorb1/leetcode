07fb37b90f2a832bec2a1602e08a31affed4145d
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-03-08 00:16:00 -0800

    Time: 67 ms (96.93%), Space: 14.3 MB (79.64%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.py
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.py
@@ -6,9 +6,6 @@
 #         self.right = right
 class Solution:
     def recoverTree(self, root: Optional[TreeNode]) -> None:
-        """
-        Do not return anything, modify root in-place instead.
-        """
         def inorder(root):
             if not root:
                 return
