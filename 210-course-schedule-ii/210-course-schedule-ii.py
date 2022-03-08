8a6b3eff33359535939c0a623ffe68e9c13ff39a
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-03-07 21:05:40 -0800

    Time: 166 ms (38.51%), Space: 17.7 MB (12.18%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,37 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        d = dict()
+        
+        for p in range(numCourses):
+            d[p] = []
+            
+        for p1, p2 in prerequisites:
+            d[p2].append(p1)
+
+        def dfs(key):
+            if key in visiting:
+                return False
+            elif key in visited:
+                return True
+            
+            visiting.add(key)
+            
+            if key in d:
+                for neighbor in d[key]:
+                    if not dfs(neighbor):
+                        return False        
+            
+            visiting.discard(key)
+            visited.add(key)
+            sol.appendleft(key)
+            return True
+        
+        sol = deque()
+        visiting, visited = set(), set()
+        
+        for key in d.keys():
+            if key not in visited:
+                if not dfs(key):
+                    return []
+                
+        return sol
