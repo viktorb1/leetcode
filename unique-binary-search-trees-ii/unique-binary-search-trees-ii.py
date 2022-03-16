5d95e48807d0882d63aa49d15a65a295afc6aaed
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-03-16 01:15:49 -0700

    Time: 72 ms (65.05%), Space: 15.7 MB (40.09%) - LeetHub

--- unique-binary-search-trees-ii/unique-binary-search-trees-ii.py
+++ unique-binary-search-trees-ii/unique-binary-search-trees-ii.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:        
+    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:     
         def dfs(left, right):
             if left > right: return [None]
             if left == right: return [TreeNode(left)]
@@ -14,11 +14,11 @@ class Solution:
             for root in range(left, right + 1):
                 leftNodes = dfs(left, root - 1)
                 rightNodes = dfs(root + 1, right)
+                
                 for l in leftNodes:
                     for r in rightNodes:
                         ans.append(TreeNode(root, l, r))
-                    
+            
             return ans
-                        
         
         return dfs(1, n)
