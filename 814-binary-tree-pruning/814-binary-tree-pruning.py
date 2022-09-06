9badbc983162433aae516593a24f4fc6c8297ea0
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-09-06 13:00:50 -0700

    Time: 69 ms (7.32%), Space: 13.9 MB (23.50%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.py
+++ 814-binary-tree-pruning/814-binary-tree-pruning.py
@@ -6,28 +6,13 @@
 #         self.right = right
 class Solution:
     def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        self.inorder(root)
-        return root if self.calc_sum(root) > 0 else None
-    
-    def inorder(self, root):
-        if self.calc_sum(root.left):
-            self.inorder(root.left)
+        if not root:
+            return None
+        
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        
+        if root.val == 0 and not root.left and not root.right:
+            return None
         else:
-            root.left = None
-        
-        if self.calc_sum(root.right):
-            self.inorder(root.right) 
-        else:
-            root.right = None
-        
-    def calc_sum(self, root):
-        if not root: return 0
-        total = root.val
-        
-        if root.left: 
-            total += self.calc_sum(root.left)
-        if root.right: 
-            total += self.calc_sum(root.right)
-        
-        return total
-        
+            return root
