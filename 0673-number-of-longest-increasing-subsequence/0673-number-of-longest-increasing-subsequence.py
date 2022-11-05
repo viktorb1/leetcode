b5da0cd063429baa111f4ef7da9489a6a8df89a3
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-11-05 12:21:25 -0700

    Time: 3765 ms (28.90%), Space: 14.3 MB (30.50%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
@@ -1,15 +1,14 @@
 class Solution:
     def findNumberOfLIS(self, nums: List[int]) -> int:
-        memo = {}
+        dp = [[1, 1] for n in nums]
         sums = defaultdict(int)
         
-        @cache
-        def dfs(i):
+        for i in range(len(nums)):
             largest_length = 1
             count = 1
             
             for j in range(i-1, -1, -1):
-                cur_len, cur_count = dfs(j)
+                cur_len, cur_count = dp[j]
                 if nums[j] < nums[i]:
                     if cur_len + 1 > largest_length:
                         largest_length = max(largest_length, cur_len + 1)
@@ -17,12 +16,8 @@ class Solution:
                     elif cur_len + 1 == largest_length:
                         count += cur_count
                     
-            memo[i] = [largest_length, count]
+            dp[i] = [largest_length, count]
             sums[largest_length] += count
-            return [largest_length, count]
         
-        
-        for i in range(len(nums)-1, -1, -1):
-            dfs(i)
         
         return sums[max(sums)]
