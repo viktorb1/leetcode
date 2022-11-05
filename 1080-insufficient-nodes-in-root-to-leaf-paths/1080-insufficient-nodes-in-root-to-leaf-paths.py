215cd7dc126061d5efd5107b9d0c3cd10b5c89e0
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-11-04 18:27:36 -0700

    Time: 293 ms (5.00%), Space: 15.4 MB (57.81%) - LeetHub

--- 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.py
+++ 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.py
@@ -8,23 +8,20 @@ class Solution:
     def sufficientSubset(self, root: Optional[TreeNode], limit: int) -> Optional[TreeNode]:
         self.paths = defaultdict(list)
         self.limit = limit
-        delete, _ = self.inorder(root)
+        delete, _ = self.inorder(root, root.val)
         return root if not delete else None
         
-    def inorder(self, node, total = set()):
-        if not node: return (None, [])
+    def inorder(self, node, total = 0):
+        if not node: return (False, [])
         if not node.left and not node.right:
-            total = total.union({node})
-            row_sum = sum([n.val for n in total])
-            should_delete = self.limit > row_sum
-            return (should_delete, [row_sum])
+            should_delete = self.limit > total
+            return (should_delete, [total])
             
-        delete_left, sums_left = self.inorder(node.left, total.union({node}))
-        delete_right, sums_right = self.inorder(node.right, total.union({node}))
+        delete_left, sums_left = self.inorder(node.left, total + node.left.val if node.left else 0)
+        delete_right, sums_right = self.inorder(node.right, total + node.right.val if node.right else 0)
         
         if delete_left: node.left = None
         if delete_right: node.right = None
                                              
-        combined = sums_left + sums_right
         should_delete = all(self.limit > val for val in sums_left + sums_right)
         return (should_delete, sums_left + sums_right)
