8d76fc6888cd157d8731bab0af024bf54a697d3f
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-11-10 16:55:15 -0800

    Time: 105 ms (54.42%), Space: 13.9 MB (80.73%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -2,21 +2,16 @@ class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
         self.min_range = float('inf')
         
-        if k == len(cookies):
-            return max(cookies)
-        
         def dfs(x):
             if x == len(cookies): 
                 self.min_range = min(max(data), self.min_range)
-                return
-            
-            if self.min_range < max(data):
                 return
             
             for j in range(k):
                 data[j] += cookies[x]
                 dfs(x+1)
                 data[j] -= cookies[x]
+                if data[j] == 0: break
         
         data = [0]*k
         dfs(0)
