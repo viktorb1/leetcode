ecc77e0099689bf19ccea3afac29917e852fec26
Victor Babchanik <vbabchanik@ucdavis.edu>
2022-11-11 17:45:52 -0800

    Time: 59 ms (60.79%), Space: 13.9 MB (91.68%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.py
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.py
@@ -6,16 +6,16 @@
 #         self.right = right
 class Solution:
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
-        parentx, depthx = self.inorder(root, x)
-        parenty, depthy = self.inorder(root, y)
+        parentx, depthx = self.preorder(root, x)
+        parenty, depthy = self.preorder(root, y)
         return parentx != parenty and depthx == depthy
         
-    def inorder(self, node, find, prev=None, level=0):
+    def preorder(self, node, find, prev=None, level=0):
         if not node:
             return None
         elif node.val == find:
             return (prev, level)
         
-        x = self.inorder(node.left, find, node, level+1)
-        y = self.inorder(node.right, find, node, level+1)
+        x = self.preorder(node.left, find, node, level+1)
+        y = self.preorder(node.right, find, node, level+1)
         return x if x else y
