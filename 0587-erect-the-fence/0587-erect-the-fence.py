43528acbd72eeea02af7bb52cf01089fca65cf56
Victor Babchanik <vbabchanik@ucdavis.edu>
2023-01-06 23:12:24 -0800

    Time: 1008 ms (8.21%), Space: 14.9 MB (6.65%) - LeetHub

--- 0587-erect-the-fence/0587-erect-the-fence.py
+++ 0587-erect-the-fence/0587-erect-the-fence.py
@@ -8,17 +8,16 @@ class Solution:
             return angle if angle < 180 else (angle - 360)
 
         if len(points) < 3: return points
-        points = [tuple(p) for p in points]
         two = first = min(points, key = lambda point: point[0]) # get leftmost
         one = (two[0], two[1]+1) # doesn't matter what we set it to, as long as it's not equal to `two`
-        hull = {two}
+        hull = [two]
 
         while True:
             ang = float('inf')
             collinear = []
 
             for three in points:
-                if three in {one, two}:
+                if three in [one, two]:
                     continue
                 
                 a = angle(one, two, three)
@@ -28,8 +27,8 @@ class Solution:
                 elif a == ang:
                     collinear.append(three)
 
-            hull = hull.union(collinear)
+            hull += collinear
             if collinear[0] == first: break
             one, two = two, collinear[0]
 
-        return hull
+        return set(tuple(h) for h in hull)
