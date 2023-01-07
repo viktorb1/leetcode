488861051a297b82b79cc570c393b45ab0f69be3
Victor Babchanik <vbabchanik@ucdavis.edu>
2023-01-07 01:30:52 -0800

    Time: 394 ms (64.83%), Space: 14.7 MB (81.12%) - LeetHub

--- 0587-erect-the-fence/0587-erect-the-fence.py
+++ 0587-erect-the-fence/0587-erect-the-fence.py
@@ -1,31 +1,42 @@
+from math import atan, atan2, pi, dist
+from typing import List, Tuple
+
 class Solution:
     def outerTrees(self, points: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
-        def angle(a, b, c):
-            angle = (atan2(a[0]-b[0],a[1]-b[1]) - atan2(c[0]-b[0],c[1]-b[1])) * 180/pi
-            return angle if angle < 180 else (angle - 360)
+        def cross_prod(x, y, z):
+            a, b, c, d, e, f = x[0], x[1], y[0], y[1], z[0], z[1]
+            return (f-b)*(c-a) - (d-b)*(e-a)
+
 
         if len(points) < 3: return points
-        two = first = min(points, key = lambda point: point[0]) # get leftmost
+        two = first = min(points, key = lambda point: (point[0], point[1])) # get leftmost
         one = (two[0], two[1]+1) # doesn't matter what we set it to, as long as it's not equal to `two`
         hull = [two]
 
         while True:
-            ang = float('inf')
-            smallest = []
+            most_counter = []
+            i = 0
 
             for three in points:
                 if three in [one, two]:
                     continue
-                
-                a = angle(one, two, three)
-                if a < ang:
-                    ang = a
-                    smallest = [three]
-                elif a == ang:
-                    smallest.append(three)
+                if not most_counter: 
+                    most_counter = [three]
+                    continue
 
-            hull += smallest
-            if smallest[0] == first: break
-            one, two = two, smallest[0]
+                c = cross_prod(two, three, most_counter[-1])
+                
+                if c > 0:
+                    i = 0
+                    most_counter = [three]
+                elif c == 0:
+                    if dist(two, most_counter[-1]) < dist(two, three) and dist(two, most_counter[0]) < dist(two, three):
+                        i = len(most_counter)
+
+                    most_counter.append(three)
+
+            hull += most_counter
+            if most_counter[i] == first: break
+            two = most_counter[i]
 
         return set(tuple(h) for h in hull)
