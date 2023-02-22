d0d8bf01a167e517b3c960c0f8b62378431c5dcd
Victor Babchanik <vbabchanik@ucdavis.edu>
2023-02-22 15:41:35 -0800

    Time: 48 ms (35.59%), Space: 14.4 MB (5.15%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        @cache
+        def dfs(target):
+            if target < 0:
+                return 0
+            elif target == 0:
+                return 1
+            
+            total = 0
+            for n in nums:
+                total += dfs(target-n)
+            return total
+        
+        return dfs(target)
