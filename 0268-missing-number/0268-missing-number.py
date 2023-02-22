b62fd5132c3f5e0597d9783839ca5fec3b9c1202
Victor Babchanik <vbabchanik@ucdavis.edu>
2023-02-21 16:46:40 -0800

    Time: 243 ms (25.96%), Space: 15.2 MB (31.20%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,3 +1,4 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         return ((len(nums) * (len(nums)+1)) // 2) - sum(nums)
+        # if you're trying to sum the first nine numbers, do n(n+1)//2, with n=9
