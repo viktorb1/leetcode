388c6469db2805fc2e9abb2149b1f921efd13770
Victor Babchanik <vbabchanik@ucdavis.edu>
2023-02-23 14:02:31 -0800

    Time: 49 ms (19.73%), Space: 14.3 MB (68.59%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+from collections import deque
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node: return
+        q = deque([node])
+        copies = {node: Node(node.val)}
+        
+        while q:
+            cur = q.popleft()
+            for n in cur.neighbors:
+                if n not in copies:
+                    copies[n] = Node(n.val)
+                    q.append(n)
+                
+                copies[cur].neighbors.append(copies[n])
+            
+        return copies[node]
