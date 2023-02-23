76c330e304d7825bc28802a860ab87a29a3a6463
Victor Babchanik <vbabchanik@ucdavis.edu>
2023-02-22 16:37:20 -0800

    Time: 35 ms (58.80%), Space: 13.9 MB (62.36%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,16 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1: return nums[0]
-        memo = {}
-        def rob_regular(arr, i):
-            if i < 0:
-                return 0
-            elif i in memo:
-                return memo[i]
+        
+        def helper(nums):
+            rob1, rob2 = 0, 0
             
-            memo[i] = max(arr[i] + rob_regular(arr, i-2), rob_regular(arr, i-1))
-            return memo[i]
-        left = rob_regular(nums[:-1], len(nums)-2)
-        memo.clear()
-        right = rob_regular(nums[1:], len(nums)-2)
-        return max(left, right)
+            for n in nums:
+                rob3 = max(rob1 + n, rob2)
+                rob1, rob2 = rob2, rob3
+            
+            return rob2
+        
+        return max(helper(nums[1:]), helper(nums[:-1]), nums[0])
