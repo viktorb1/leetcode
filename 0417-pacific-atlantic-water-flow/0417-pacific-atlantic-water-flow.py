0ec6fb715b21d824b536147ee3e5b407a98603fc
Victor Babchanik <vbabchanik@ucdavis.edu>
2023-02-27 13:58:31 -0800

    Time: 281 ms (77.06%), Space: 15.7 MB (32.67%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,23 +1,21 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        pacific, atlantic = set(), set()
         rows = len(heights)
         cols = len(heights[0])
+        atlantic_visited, pacific_visited = set(), set()
         
+        def climb(i, j, p_or_a):
+            p_or_a.add((i,j))
+            for (x,y) in [(i-1,j),(i+1, j),(i,j-1),(i,j+1)]:
+                if 0 <= x < rows and 0 <= y < cols and (x,y) not in p_or_a and heights[x][y] >= heights[i][j]:
+                    climb(x,y,p_or_a)
         
-        def climb(i, j, set_container):
-            set_container.add((i,j))
-            for (x, y) in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
-                if (0 <= x < rows) and (0 <= y < cols) and heights[i][j] <= heights[x][y] and (x, y) not in set_container:
-                    climb(x, y, set_container)
-            
-
         for r in range(rows):
-            climb(r, 0, pacific)
-            climb(r, cols-1, atlantic)
+            climb(r, 0, pacific_visited)
+            climb(r, cols-1, atlantic_visited)
         
         for c in range(cols):
-            climb(0, c, pacific)
-            climb(rows-1, c, atlantic)
+            climb(0, c, pacific_visited)
+            climb(rows-1, c, atlantic_visited)
         
-        return pacific.intersection(atlantic)
+        return atlantic_visited.intersection(pacific_visited)
