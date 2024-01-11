34af3697e05175963f84499d299c7ad6e7e5bb39
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-01-10 20:37:20 -0800

    Time: 47 ms (42.07%), Space: 19.1 MB (74.81%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.py
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.py
@@ -1,34 +1,26 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
         ans = 0
+
         def postorder(node):
             nonlocal ans
-            
-            if not node: return -1, -1
-            
+
+            if not node:
+                return float('inf'), float('-inf')
+
             l_sm, l_lg = postorder(node.left)
             r_sm, r_lg = postorder(node.right)
-            
-            if l_sm == -1: l_sm = node.val
-            if l_lg == -1: l_lg = node.val
-            if r_sm == -1: r_sm = node.val
-            if r_lg == -1: r_lg = node.val
-            
+
+            node_min = min(l_sm, r_sm, node.val)
+            node_max = max(l_lg, r_lg, node.val)
+
             max_diff = max(
-                abs(node.val - l_sm), 
-                abs(node.val - l_lg),
-                abs(node.val - r_sm),
-                abs(node.val - r_lg)
+                abs(node.val - node_min),
+                abs(node.val - node_max)
             )
-            
+
             ans = max(max_diff, ans)
-            return min(l_sm, r_sm, node.val), max(l_lg, r_lg, node.val)
-    
+            return node_min, node_max
+
         postorder(root)
         return ans
