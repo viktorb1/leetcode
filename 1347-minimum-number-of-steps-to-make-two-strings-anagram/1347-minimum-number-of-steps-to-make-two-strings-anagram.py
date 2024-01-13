674ea4f4b82ee1c3c713c276239580c9d288a183
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-01-12 16:19:46 -0800

    Time: 87 ms (99.35%), Space: 17.9 MB (21.41%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -4,8 +4,6 @@ class Solution:
         
         for c, count in c_t.items():
             if c in c_s:
-                min_count = min(c_s[c], c_t[c])
-                c_s[c] -= min_count
-                c_t[c] -= min_count
+                c_s[c] -= min(c_s[c], c_t[c])
       
         return sum(c_s.values())
