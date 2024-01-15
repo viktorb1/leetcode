501249b9725e72909a3d2a1044e53a42edb09cef
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-01-15 12:59:33 -0800

    Time: 1397 ms (90.54%), Space: 71.5 MB (55.37%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -3,8 +3,6 @@ class Solution:
         losers_list = [l for _, l in matches]
         losers_counter = Counter(losers_list)
         winners = set([w for w, _ in matches])
-        
         ans_1 = sorted(list(winners - losers_counter.keys()))
         ans_2 = sorted([l for l, c in losers_counter.items() if c == 1])
-
         return [ans_1, ans_2]
