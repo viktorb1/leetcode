ad3f84ae394510e3406a049005c8481008cc58e1
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-01-23 20:16:50 -0800

    Time: 523 ms (68.89%), Space: 43.5 MB (98.67%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+        count = 0
+        
+        def dfs(node, c):
+            nonlocal count
+            
+            if not node.left and not node.right:
+                if self.is_palindrome(c):
+                    count += 1
+            
+            if node.left:
+                c[node.left.val] += 1
+                dfs(node.left, c)
+                c[node.left.val] -= 1
+            
+            if node.right:
+                c[node.right.val] += 1
+                dfs(node.right, c)
+                c[node.right.val] -= 1
+        
+        dfs(root, Counter([root.val]))
+        return count
+            
+    def is_palindrome(self, c):
+        c = c.values()        
+        return len([d for d in c if d % 2 == 1]) <= 1
