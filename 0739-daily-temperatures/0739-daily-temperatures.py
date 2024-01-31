f97a1c1b5ebd72c9108c4abc371fb343e9b92f6d
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-01-30 18:49:05 -0800

    Time: 877 ms (90.32%), Space: 31.4 MB (68.42%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -4,9 +4,13 @@ class Solution:
         ans = [0] * len(T)
         
         for i in range(len(T)):
-            while stack and T[stack[-1]] < T[i]:
+            while stack and T[stack[-1]] < T[i]: 
                 p = stack.pop()
                 ans[p] = i - p
             stack.append(i)
         
         return ans
+# you can think of T[i] pointer as moving faster than the stack pointer
+# this stack removes everything smaller than T[i]
+# this means that the values in the stack have not seen anything bigger than themselves until T[i] (which means it's the next greatest element)
+# this is an example of a monotonic decreasing stack since we are removing everything smaller than itself before add it
