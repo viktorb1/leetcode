46d1bf47e4694e372acdf529a3d045eca56cc866
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-02-03 07:20:26 -0800

    Time: 376 ms (58.95%), Space: 16.7 MB (72.76%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -4,7 +4,6 @@ class Solution:
         
         for i in range(k, len(dp)):
             max_k = dp[i]
-            max_val = float('-inf')
             for j in range(1, k+1):
                 if i-k-j+1 >= 0:
                     max_k = max(max_k, arr[i-k-j+1])
