9f21192c96acb88e77dea4592b335fd7e2ecb79d
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-02-12 20:39:58 -0800

    Time: 54 ms (34.51%), Space: 16.9 MB (56.41%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -2,21 +2,18 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         @cache
         def recur(s, p):
+            
+            if len(p) > 1 and p[1] == "*" and recur(s, p[2:]):
+                return True
+            
             if not s and not p:
                 return True
-            elif s and not p:
+            elif not s or not p:
                 return False
-            elif not s and p:
-                if len(p) > 1 and p[1] == "*":
-                    return recur(s, p[2:])
-                else:
-                    return False
             
+
             if p[0] == ".":
                 if len(p) > 1 and p[1] == "*":
-                    if recur(s, p[2:]):
-                        return True
-                
                     for i, c in enumerate(s):
                         if recur(s[i+1:], p[2:]):
                             return True
@@ -25,9 +22,6 @@ class Solution:
             elif len(p) > 1 and p[1] == "*":
                 if p[0] != s[0]:
                     return recur(s, p[2:])
-                
-                if recur(s, p[2:]):
-                    return True
                 
                 for i, c in enumerate(s):
                     if c != s[0]:
