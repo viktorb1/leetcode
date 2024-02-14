6aed372b3ee0444a492a769e43295a306fb41cb4
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-02-13 18:48:43 -0800

    Time: 986 ms (95.44%), Space: 48.1 MB (30.11%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -2,17 +2,12 @@ from queue import Queue
 
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        p, n = 0, 0
-        ans = [0] * len(nums)
+        pos, neg = [], []
         
-        for i, a in enumerate(ans):
-            if i % 2 == 0:
-                while nums[p] < 0: p += 1
-                ans[i] = nums[p]
-                p += 1
+        for n in nums:
+            if n > 0:
+                pos.append(n)
             else:
-                while nums[n] > 0: n += 1
-                ans[i] = nums[n]
-                n += 1
+                neg.append(n)
         
-        return ans
+        return [item for pair in zip(pos, neg) for item in pair]
