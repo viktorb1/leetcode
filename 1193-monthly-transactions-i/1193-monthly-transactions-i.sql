06d2cfe9d82215f43deb0259936c200f07ef74e6
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-02-16 14:49:19 -0800

    Time: 1115 ms (45.83%), Space: 0B (100.00%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -3,8 +3,8 @@ SELECT
     CONCAT(YEAR(trans_date), '-', LPAD(MONTH(trans_date), 2, '0')) as month,
     country,
     COUNT(*) as trans_count,
-    SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) as approved_count,
+    SUM(IF(state = 'approved', 1, 0)) as approved_count,
     SUM(amount) as trans_total_amount,
-    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) as approved_total_amount
+    SUM(IF(state = 'approved', amount, 0)) as approved_total_amount
 FROM Transactions
 GROUP BY month, country
