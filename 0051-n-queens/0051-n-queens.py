900ae776cd9b18cfb504ea0d4421b203c199ac5d
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-02-19 18:29:23 -0800

    Time: 74 ms (38.46%), Space: 17.1 MB (71.13%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,31 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        cols = [-1] * n
+        ans = []
+        
+        def dfs(cols, path):
+            nonlocal ans, n
+            r = len(path)
+            if r == n:
+                ans.append(path)
+                return
+            
+            
+            for c in range(n):
+                cols[r] = c
+                
+                if is_valid(cols, r):
+                    row = "." * n
+                    dfs(cols, path + [row[:c] + "Q" + row[c+1:]])
+                    
+        
+        def is_valid(cols, r):
+            for i in range(r):
+                if abs(cols[i] - cols[r]) == r - i or cols[i] == cols[r]:
+                    return False
+            
+            return True
+        
+        dfs([-1]*n, [])
+        return ans
+
