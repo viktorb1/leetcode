8f23868a262a0d101c426947247a415098a586f7
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-02-22 22:07:41 -0800

    Time: 89 ms (88.65%), Space: 17.9 MB (80.88%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,7 +1,8 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         g = defaultdict(dict) # dict for representing graph
-        for f, t, p in flights: g[f][t] = p
+        for f, t, p in flights: 
+            g[f][t] = p
         
         heap = [(0, src, 0)] # heap to get minimum
         dist = [float('inf') for _ in range(n)]
@@ -13,11 +14,12 @@ class Solution:
                 return w
             
             if dist[u] < count:
+                # since minheap is sorted by increasing weight, the last time we saw `u` it must have had a lower (or equal) weight
+                # the count was also smaller then, so no way this new path is as good
                 continue
             
             dist[u] = count
             
-
             if count <= k:
                 for v, weight in g[u].items():
                     heappush(heap, (w + weight, v, count + 1))
