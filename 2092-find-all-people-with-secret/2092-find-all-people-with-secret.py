8e76bd12a488092772b79fe98da6785f18c6bfac
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-02-23 19:12:16 -0800

    Time: 1798 ms (29.80%), Space: 68.9 MB (20.53%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -1,42 +1,25 @@
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-        graphs = defaultdict(dict)
+        q = [(0, 0), (0, firstPerson)]
+        graph = defaultdict(list)
         
-        for x, y, t in meetings:
-            if x not in graphs[t]:
-                graphs[t][x] = set()
-            if y not in graphs[t]:
-                graphs[t][y] = set()
-            
-            graphs[t][x].add(y)
-            graphs[t][y].add(x)
+        for p1, p2, t in meetings:
+            graph[p1].append((p2, t))
+            graph[p2].append((p1, t))
         
-        know = {0, firstPerson}
+        seen = set()
+        while q:
+            t, p1 = heappop(q)
+            if p1 in seen:
+                continue
+            seen.add(p1)
+            
+            for p2, mt in graph[p1]:
+                if mt >= t:
+                    heappush(q, (mt, p2))
         
-        def flood(g, x, know):
-            q = deque([x])
-            
-            while q:
-                for i in range(len(q)):
-                    n1 = q.popleft()
-                    
-                    for n2 in g[n1]:
-                        if n2 not in know:
-                            know.add(n2)
-                            q.append(n2)
+        return seen
         
-        seen_t = set()
-
-        for x,y,t in sorted(meetings, key=lambda x: x[2]):
-            if x in know and (x, t) not in seen_t:
-                flood(graphs[t], x, know)
-            
-            if y in know and (y, t) not in seen_t:
-                flood(graphs[t], y, know)
-            
-            seen_t.add((x,t))
-            seen_t.add((y,t))
-            
         
         
         return know
