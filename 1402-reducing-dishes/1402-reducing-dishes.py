3bd9e65e9f4ca8438b2c4412861592e2f364e367
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-02-24 00:46:28 -0800

    Time: 45 ms (63.16%), Space: 16.5 MB (98.98%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,18 +1,10 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
-        satisfaction.sort()
-        
-        memo = {}
-        def dfs(sat, c):
-            if not sat:
-                return 0
-            
-            if (len(sat), c) in memo:
-                return memo[(len(sat), c)]
-            
-            maxval = dfs(sat[1:], c)
-            maxval = max(maxval, sat[0]*c + dfs(sat[1:], c+1))
-            memo[(len(sat), c)] = maxval
-            return maxval
-        
-        return dfs(satisfaction, 1)
+        satisfaction.sort(reverse = True)
+        stsum, ans = 0, 0
+        for dish in satisfaction:
+            stsum += dish
+            if stsum < 0:
+                break
+            ans += stsum
+        return ans
