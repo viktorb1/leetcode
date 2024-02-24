1b7452148db5db29d5267c5148dc33dbef498649
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-02-23 19:52:09 -0800

    Time: 1647 ms (49.67%), Space: 66.7 MB (22.51%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -1,22 +1,20 @@
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-
+        q = [(0, 0), (0, firstPerson)]
         graph = defaultdict(list)
-        for x, y, t in meetings:
-            graph[x].append((t, y))
-            graph[y].append((t, x))
-
-        earliest = [inf] * n
-        earliest[0] = 0
-        earliest[firstPerson] = 0
-
-        def dfs(person, time):
-            for t, nextPerson in graph[person]:
-                if t >= time and earliest[nextPerson] > t:
-                    earliest[nextPerson] = t
-                    dfs(nextPerson, t)
-
-        dfs(0, 0)
-        dfs(firstPerson, 0)
         
-        return [i for i in range(n) if earliest[i] != inf]
+        for p1, p2, t in meetings:
+            graph[p1].append((p2, t))
+            graph[p2].append((p1, t))
+        
+        seen = [inf] * n
+        while q:
+            t, p1 = heappop(q)
+            seen[p1] = t
+            
+            for p2, mt in graph[p1]:
+                if mt >= t and seen[p2] > mt:
+                    seen[p2] = mt
+                    heappush(q, (mt, p2))
+        
+        return [i for i,v in enumerate(seen) if v != inf]
