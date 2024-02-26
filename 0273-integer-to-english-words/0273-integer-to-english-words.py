c9ac2a04826780da21e96e1b3e013f6facc6aa6e
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-02-26 12:34:18 -0800

    Time: 51 ms (6.97%), Space: 16.5 MB (93.80%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.py
+++ 0273-integer-to-english-words/0273-integer-to-english-words.py
@@ -15,16 +15,22 @@ class Solution:
             z = int(num[i]) if i < len(num) else 0
             section = []
             
-            if x > 0: section.append(o[x-1])
-            if x > 0: section.append(p[0])
-            if y > 0: 
-                if y == 1:
-                    section.append(d[z])
-                else:
-                    section.append(t[y-1])
+            if x > 0: 
+                section.append(o[x-1])
+                section.append(p[0])
             
-            if y != 1 and z > 0: section.append(o[z-1])
-            if i > 0 and any([x,y,z]): section.append(p[i//3])
-            ans.appendleft(' '.join(section))
+            if y == 1:
+                section.append(d[z])
+            elif y > 0:
+                section.append(t[y-1])
             
-        return ' '.join([a for a in ans if a != '']).strip()
+            if y != 1 and z > 0:
+                section.append(o[z-1])
+            
+            if i > 0 and any([x,y,z]):
+                section.append(p[i//3])
+            
+            if section:
+                ans.appendleft(' '.join(section))
+            
+        return ' '.join(ans).strip()
