e4f1482c3c6fd91e0cae64168edc381dee2f97ba
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-02-26 12:31:17 -0800

    Time: 43 ms (25.42%), Space: 16.6 MB (93.80%) - LeetHub

---
+++ 0273-integer-to-english-words/0273-integer-to-english-words.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numberToWords(self, num: int) -> str:
+        p = ["Hundred", "Thousand", "Million", "Billion"]
+        t = ["Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+        o = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+        d = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+        
+        if not num: return "Zero"
+        num = str(num)[::-1]
+        ans = deque()
+        
+        for i in range(0, len(num), 3):
+            x = int(num[i+2]) if i+2 < len(num) else 0
+            y = int(num[i+1]) if i+1 < len(num) else 0
+            z = int(num[i]) if i < len(num) else 0
+            section = []
+            
+            if x > 0: section.append(o[x-1])
+            if x > 0: section.append(p[0])
+            if y > 0: 
+                if y == 1:
+                    section.append(d[z])
+                else:
+                    section.append(t[y-1])
+            
+            if y != 1 and z > 0: section.append(o[z-1])
+            if i > 0 and any([x,y,z]): section.append(p[i//3])
+            ans.appendleft(' '.join(section))
+            
+        return ' '.join([a for a in ans if a != '']).strip()
