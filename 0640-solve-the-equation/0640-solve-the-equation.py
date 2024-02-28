c229d915360935337c9ebaeae5c078ff6345effa
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-02-27 21:08:04 -0800

    Time: 35 ms (60.57%), Space: 16.6 MB (71.43%) - LeetHub

--- 0640-solve-the-equation/0640-solve-the-equation.py
+++ 0640-solve-the-equation/0640-solve-the-equation.py
@@ -9,32 +9,28 @@ class Solution:
                 x_diff, num_diff = add(split[0])
                 x_sum += x_diff
                 num_sum += num_diff
-                print(split)
-                if len(split) > 1: 
-                    for s in split[1:]:
-                        x_diff, num_diff = add(s)
-                        x_sum -= x_diff
-                        num_sum -= num_diff
+
+                for chunk in split[1:]:
+                    x_diff, num_diff = add(chunk)
+                    x_sum -= x_diff
+                    num_sum -= num_diff
             
             return x_sum, num_sum
         
-        def add(split, num_sum=0, x_sum=0):
-            if split == "": return 0, 0
+        def add(chunk, x_sum = 0, num_sum = 0):
+            if chunk == "": return 0, 0
             try:
-                num_sum = int(split)
+                return 0, int(chunk)
             except:
                 try:
-                    x_sum = int(split[:-1])
+                    return int(chunk[:-1]), 0
                 except:
-                    x_sum = 1
-            return x_sum, num_sum
+                    return 1, 0
         
         
         left, right = equation.split('=')
         l_x_sum, l_num_sum = parseEq(left)
         r_x_sum, r_num_sum = parseEq(right)
-        print(l_x_sum, l_num_sum)
-        print(r_x_sum, r_num_sum)
         
         if l_x_sum == r_x_sum and l_num_sum == r_num_sum:
             return "Infinite solutions"
@@ -44,4 +40,3 @@ class Solution:
             return "x=0"
         else:
             return f'x={(r_num_sum - l_num_sum) // (l_x_sum - r_x_sum)}'
-    
