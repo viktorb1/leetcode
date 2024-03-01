3059de549588ad4509031780dac3e9919c765eed
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-02-29 21:11:23 -0800

    Time: 46 ms (7.09%), Space: 16.8 MB (41.85%) - LeetHub

---
+++ 0068-text-justification/0068-text-justification.py
@@ -0,0 +1,38 @@
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        
+        sol = []
+        row = ""
+        for w1, w2 in zip(words, words[1:] + [""]):
+            row += w1
+            
+            if len(row) < maxWidth:
+                row += " "
+            
+            if len(row) + len(w2) > maxWidth:
+                sol.append(row)
+                row = ""
+        
+        sol.append(row)
+        
+        for i in range(len(sol)):
+            char_count = len(sol[i].strip())
+            sol[i] = sol[i].strip().split()
+            char_count -= (len(sol[i]) - 1)
+            dividors = len(sol[i])
+            rem = maxWidth - char_count
+            
+            if i == len(sol) - 1:
+                sol[i] = ' '.join(sol[i]) + ' '*(rem-len(sol[i])+1)
+                break
+            
+            j = 0
+            while rem:
+                j %= len(sol[i])-1 if len(sol[i]) > 1 else len(sol[i])
+                sol[i][j] = sol[i][j] + " "
+                rem -= 1
+                j += 1
+            
+            sol[i] = ''.join(sol[i])
+        
+        return sol
