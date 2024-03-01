c97eee93a011d113865a9aba6ef8ab401b630d4a
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-02-29 20:17:59 -0800

    Time: 6595 ms (5.03%), Space: 81.8 MB (5.37%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -0,0 +1,19 @@
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        nums = [str(i+1) for i in range(n)]
+        solutions = []
+        
+        def dfs(arr, ans=[]):
+            nonlocal solutions
+            if len(solutions) >= k:
+                return
+            if len(ans) == len(nums):
+                solutions.append(ans)
+                return
+
+            
+            for i, choose in enumerate(arr):
+                dfs(arr[:i] + arr[i+1:], ans + [choose])
+        
+        dfs(nums)
+        return "".join(solutions[k-1])
