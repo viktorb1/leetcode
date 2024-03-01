38a027a4c012b24798659d9ea69c208efcd3a9db
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-02-29 17:47:37 -0800

    Time: 957 ms (61.58%), Space: 17.7 MB (31.91%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        n = len(words[0])
+        sol = []
+        
+        c1 = Counter(words)
+
+        for offset in range(n):
+            s_mod = [s[i:i+n] for i in range(offset, len(s), n)]
+
+            for i, w in enumerate(s_mod):
+                if i+len(words) > len(s_mod):
+                    break
+
+                c2 = Counter(s_mod[i:i+len(words)])
+
+                if c1 == c2:
+                    sol.append(i*n + offset)
+        
+        return sol
+        
