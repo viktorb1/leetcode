507a745f5ab8bc138a0398ac816ab18c9ec64525
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-02-29 20:29:36 -0800

    Time: 4666 ms (5.57%), Space: 16.4 MB (97.25%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,6 +1,6 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        nums = [str(i+1) for i in range(n)]
+        nums = tuple(str(i+1) for i in range(n))
         count = 0        
         
         
