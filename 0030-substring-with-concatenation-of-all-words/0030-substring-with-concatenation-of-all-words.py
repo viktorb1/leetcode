3efe8fa61b3a65970a2737c25ad5f2ecda61e370
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-02-29 17:53:00 -0800

    Time: 953 ms (61.60%), Space: 17.5 MB (34.60%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -2,15 +2,13 @@ class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         n = len(words[0])
         sol = []
-        
         c1 = Counter(words)
 
         for offset in range(n):
-            s_mod = [s[i:i+n] for i in range(offset, len(s), n)]
-
+            s_split = [s[i:i+n] for i in range(offset, len(s), n)]
             i = 0
-            while i+len(words) <= len(s_mod):
-                c2 = Counter(s_mod[i:i+len(words)])
+            while i+len(words) <= len(s_split):
+                c2 = Counter(s_split[i:i+len(words)])
                 if c1 == c2: sol.append(i*n + offset)
                 i += 1
         
