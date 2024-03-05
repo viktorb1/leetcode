751770b67c2c1856427addc4e3931e9c21ff92fd
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-03-04 16:43:49 -0800

    Time: 1200 ms (32.75%), Space: 54.5 MB (8.77%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.py
+++ 0815-bus-routes/0815-bus-routes.py
@@ -2,7 +2,7 @@ class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
         if source == target: return 0
         routes = [set(r) for r in routes]
-        seen = set()
+        seen_stops, seen_buses = set(), set()
         count = 0
         
         g = defaultdict(set)
@@ -15,15 +15,16 @@ class Solution:
             count += 1
             for _ in range(len(q)):
                 bus_stop = q.popleft()
+                seen_stops.add(bus_stop)
                 
                 for i in g[bus_stop]:
-                    if i in seen: continue
-                    seen.add(i)
+                    if i in seen_buses: continue
+                    seen_buses.add(i)
                     for bus_stop_neighbor in routes[i]:
-                        if bus_stop_neighbor == bus_stop: continue
                         if bus_stop_neighbor == target:
                             return count
                         else:
-                            q.append(bus_stop_neighbor)
+                            if bus_stop_neighbor not in seen_stops:
+                                q.append(bus_stop_neighbor)
             
         return -1
