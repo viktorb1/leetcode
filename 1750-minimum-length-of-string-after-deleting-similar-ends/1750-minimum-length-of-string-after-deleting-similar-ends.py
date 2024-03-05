24a6cba4307663de6f25d7caf9630a2d6b855e80
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-03-04 18:06:16 -0800

    Time: 69 ms (60.66%), Space: 17.4 MB (75.83%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
@@ -3,14 +3,12 @@ class Solution:
         l, r = 0, len(s)-1
         if s[l] != s[r]: return len(s)
         
-        while l < r:
+        while l < r and s[r] == s[l]:
             while s[l] == s[r] and l < r:
                 l += 1
             
             while s[r] == s[l-1] and l <= r:
                 r -= 1
-            
-            if s[r] != s[l]:
-                break
+
         
         return r - l + 1
