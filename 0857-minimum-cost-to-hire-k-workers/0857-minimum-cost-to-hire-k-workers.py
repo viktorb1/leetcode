f30202cb5a7f027aebf8b107272d9c7cde723d69
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-03-05 15:45:15 -0800

    Time: 636 ms (5.32%), Space: 19 MB (43.35%) - LeetHub

--- 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
@@ -1,18 +1,25 @@
 class Solution:
     def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
-        workers = sorted([(w/q, q) for w, q in zip(wage, quality)])
-        min_cost = float('inf')
-        total_quality = 0
-        max_heap = []
-
-        for ratio, q in workers:
-            total_quality += q
-            heappush(max_heap, -q)
-
-            if len(max_heap) > k:
-                total_quality += heappop(max_heap)
-
-            if len(max_heap) == k:
-                min_cost = min(min_cost, ratio * total_quality)
-
-        return min_cost
+        # someone will get paid their target wage, because if everyone got paid over their asking wage
+        # then the solution is not optimal, since we can bring down everyone's salary little by little
+        # until someone's salary hits their target salary
+        
+        # worker i's wage can be calculated from the captain's
+        # worker i's wage =  (quality[i] / quality[captain]) * wage[captain]
+        # let's call wage[captain] / quality[captain] the captain's ratio
+        # using the ratio, we can calculate everyone else's expected wage and take the lowest k expected wages
+        min_payment = float('inf')
+        workers = sorted([(w/q, q, w) for q, w in zip(quality, wage)])
+        wage_offer = []
+        
+        for i, (captain_ratio, q, w) in enumerate(workers):
+            heappush(wage_offer, -q)
+            # -q because we want to get rid of the highest paid worker remember quanity*captain_ratio = wage_offer
+            
+            if len(wage_offer) > k:
+                heappop(wage_offer)
+            
+            if len(wage_offer) == k:
+                min_payment = min(min_payment, -captain_ratio*sum(wage_offer))
+        
+        return min_payment
