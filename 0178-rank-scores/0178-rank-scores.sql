578696017e5b8be896faad1752776d6dc2b8b475
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-03-05 22:20:38 -0800

    Time: 623 ms (37.54%), Space: 0B (100.00%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,6 +1,6 @@
 SELECT score, 
-       (SELECT COUNT(DISTINCT score) 
-        FROM Scores s2 
+       (SELECT COUNT(score) 
+        FROM (SELECT DISTINCT score FROM Scores) AS s2
         WHERE s2.score >= s1.score) AS 'rank'
 FROM Scores s1
 ORDER BY score DESC;
