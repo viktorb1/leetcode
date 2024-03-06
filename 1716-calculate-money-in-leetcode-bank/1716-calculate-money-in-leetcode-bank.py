0ae4d1a0537e9bdb5848afb7ed6cfc00317b0a33
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-03-05 18:59:46 -0800

    Time: 37 ms (52.84%), Space: 16.6 MB (68.46%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -4,9 +4,8 @@ class Solution:
         weeks = n//7 + 1
         m = 7
         
-        for w in range(weeks-1):
+        for w in range(weeks):
+            if w == weeks-1: m = n % 7
             total += w*m + (m*(m+1))//2
         
-        m = n % 7
-        total += (weeks-1)*m + (m*(m+1))//2
         return total
