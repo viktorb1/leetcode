6800e4c85a037f326b8f38d5f6933b42cbe329f4
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-03-05 19:32:41 -0800

    Time: 45 ms (54.72%), Space: 16.7 MB (88.61%) - LeetHub

---
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        count = Counter(answers)
+        total = 0
+        
+        for c, v in count.items():
+            max_in_a_group = c + 1
+            groups = ceil(v / max_in_a_group)
+            total += (c+1)*groups
+        
+        return total
