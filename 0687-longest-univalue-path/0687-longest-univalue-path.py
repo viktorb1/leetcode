6f7d9a8a3207489ccfdbc4c099dd17ad2686b312
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-03-06 23:21:02 -0800

    Time: 200 ms (30.71%), Space: 19.1 MB (99.60%) - LeetHub

--- 0687-longest-univalue-path/0687-longest-univalue-path.py
+++ 0687-longest-univalue-path/0687-longest-univalue-path.py
@@ -6,20 +6,16 @@
 #         self.right = right
 class Solution:
     def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
-        if not root: return 0
-        dist1 = self.longestUnivaluePath(root.left)
-        dist2 = self.longestUnivaluePath(root.right)
-        hl = self.height(root.left, root.val) + 1 if root.left and root.left.val == root.val else 0
-        hr = self.height(root.right, root.val) + 1 if root.right and root.right.val == root.val else 0
-        return max(dist1, dist2, hl+hr)
-
-    def height(self, node, val):
-        if not node: return 0
-        l, r = 0, 0
-        if node.left and node.left.val == val:
-            l = 1 + self.height(node.left, val)
-
-        if node.right and node.right.val == val:
-            r = 1 + self.height(node.right, val)
-
-        return max(l, r)
+        longest = [0]
+        
+        def traverse(node):
+            if not node:
+                return 0
+            left_len, right_len = traverse(node.left), traverse(node.right)
+            left = (left_len + 1) if node.left and node.left.val == node.val else 0
+            right = (right_len + 1) if node.right and node.right.val == node.val else 0
+            longest[0] = max(longest[0], left + right)
+            return max(left, right)
+        
+        traverse(root)
+        return longest[0]
