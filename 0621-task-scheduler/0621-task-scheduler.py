9c3495a49f469c98d7623975e541b838d29e7f11
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-03-06 20:22:03 -0800

    Time: 739 ms (7.15%), Space: 17 MB (95.84%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -0,0 +1,22 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        min_int = 0
+        q = [(0, -v, k) for k, v in Counter(tasks).items()]
+        heapify(q)
+        cur_time = 0
+        
+        while q:
+            t = q[0][0]
+            
+            
+            if t <= cur_time:
+                t, v, k = max([(t, v, k) for t, v, k in q if t <= cur_time], key=lambda x: -x[1])
+                q.remove((t, v, k))
+                heapify(q)
+                # heappop(q)
+                if v+1 < 0:
+                    heappush(q, (cur_time + n + 1, v+1, k))
+            
+            cur_time += 1
+        
+        return cur_time
