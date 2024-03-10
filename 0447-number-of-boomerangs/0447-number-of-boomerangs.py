fe518810a3f94342b5fc76fbf4cf0f1b2339c518
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-03-09 23:55:37 -0800

    Time: 456 ms (69.55%), Space: 16.9 MB (67.76%) - LeetHub

--- 0447-number-of-boomerangs/0447-number-of-boomerangs.py
+++ 0447-number-of-boomerangs/0447-number-of-boomerangs.py
@@ -5,7 +5,7 @@ class Solution:
         for j, k in points:
             d = defaultdict(int)
             for l , m in points:
-                dist = sqrt(pow(l-j, 2) + pow(m-k, 2))
+                dist = pow(l-j, 2) + pow(m-k, 2)
                 d[dist] += 1
             
             for v in d.values():
