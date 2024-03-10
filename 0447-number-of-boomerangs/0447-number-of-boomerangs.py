61a6db704a8d3f77035e08541cc81e2f6ad8d923
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-03-09 23:50:16 -0800

    Time: 622 ms (28.96%), Space: 16.9 MB (67.76%) - LeetHub

---
+++ 0447-number-of-boomerangs/0447-number-of-boomerangs.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfBoomerangs(self, points: List[List[int]]) -> int:
+        total = 0
+        
+        for j, k in points:
+            d = defaultdict(int)
+            for l , m in points:
+                dist = sqrt(pow(l-j, 2) + pow(m-k, 2))
+                d[dist] += 1
+            
+            for k, v in d.items():
+                if v > 1:
+                    total += (v * (v-1))
+
+                
+        
+        return total
