be7de100f19b52e2d7c7f5c501c27f3fa6cd7631
Victor Babchanik <vbabchanik@ucdavis.edu>
2024-03-26 15:40:52 -0700

    Time: 317 ms (16.45%), Space: 36.44 MB (7.57%) - LeetCodeSync

---
+++ 0041_HARD_First_Missing_Positive/1214873018_0041_First_Missing_Positive.py
@@ -0,0 +1,18 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums = list(set(nums))
+        nmin, nmax = float('inf'), float('-inf')
+
+        for n in nums:
+            if n > 0:
+                nmin = min(nmin, n)
+                nmax = max(nmax, n)
+
+        if nmin > 1: return 1
+        rem = [0 for _ in range(len(nums))]
+
+        for n in nums:
+            if 0 < n <= len(nums):
+                rem[n-1] = 1
+        
+        return rem.index(0) + 1 if sum(rem) != len(nums) else nmax+1
